2364535978463a8a1d822a72ef1f6b68b0e61558
alym karim <akarim@openstudycollege.com>
2022-05-31 16:19:57 +0100

    feat: test change

--- packages/osc-academic-hub/app/root.tsx
+++ packages/osc-academic-hub/app/root.tsx
@@ -44,6 +44,7 @@ export default function App() {
         <Links />
       </head>
       <body className="h-full">
+        <p> test change </p>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
