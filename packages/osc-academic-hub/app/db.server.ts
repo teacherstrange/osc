a303c1077b252816ee53abafdb4ab3c350071de9
alym karim <akarim@openstudycollege.com>
2022-06-10 20:37:38 +0100

    fix: added datasource to prismaclient

--- packages/osc-academic-hub/app/db.server.ts
+++ packages/osc-academic-hub/app/db.server.ts
@@ -10,8 +10,14 @@ declare global {
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 // in production we'll have a single connection to the DB.
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+if (process.env.NODE_ENV === "development") {
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 } else {
   if (!global.__db__) {
     global.__db__ = new PrismaClient();
