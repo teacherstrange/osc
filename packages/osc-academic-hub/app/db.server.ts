99a410d70f9657b02f0da29df4f9d01ae8303a0c
alym karim <akarim@openstudycollege.com>
2022-06-14 09:13:06 +0100

    fix: generate server-ca.pem everytime prisma is initialized

--- packages/osc-academic-hub/app/db.server.ts
+++ packages/osc-academic-hub/app/db.server.ts
@@ -1,10 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import { tmpdir } from "os";
+import path from "path";
 
 let prisma: PrismaClient;
 
 declare global {
   var __db__: PrismaClient;
 }
+
+fs.writeFile(
+  path.join(process.cwd(), `/server-ca.pem`),
+  process.env.CLIENT_CERTIFICATE!.replace(/\n/g, " "),
+  (err) => {
+    if (err) return console.log(err);
+  }
+);
+
+console.log(path.join(process.cwd(), `/server-ca.pem`));
 
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
@@ -15,6 +28,7 @@ if (process.env.NODE_ENV === "production") {
     process.env.NODE_ENV,
     process.env.PLANETSCALE_PRISMA_DATABASE_URL
   );
+
   prisma = new PrismaClient({
     datasources: {
       db: {
@@ -24,7 +38,13 @@ if (process.env.NODE_ENV === "production") {
   });
 } else {
   if (!global.__db__) {
-    global.__db__ = new PrismaClient();
+    global.__db__ = new PrismaClient({
+      datasources: {
+        db: {
+          url: process.env.PLANETSCALE_PRISMA_DATABASE_URL,
+        },
+      },
+    });
   }
   prisma = global.__db__;
   prisma.$connect();
