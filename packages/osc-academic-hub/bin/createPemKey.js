c5aab0f2b1d8ed6178fcf8343e09920822529869
alym karim <akarim@openstudycollege.com>
2022-06-15 04:44:43 +0100

    fix: added create pem file command to start.sh

---
+++ packages/osc-academic-hub/bin/createPemKey.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const crypto = require("crypto");
+const fs = require("fs");
+const { encrypted } = require("../prisma/server-ca-enc");
+
+const algorithm = "aes-128-cbc";
+const decipher = crypto.createDecipheriv(
+  algorithm,
+  "jc7xh9d2lvw2uluo",
+  "uf0k0dx55rp7u1sg"
+);
+
+const getDecryptedSecret = () => {
+  let decrypted = decipher.update(encrypted, "base64", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+};
+
+fs.writeFile(
+  path.join(process.cwd(), `/prisma/server-ca.pem`),
+  getDecryptedSecret(),
+  "base64",
+  (err) => {
+    if (err) return console.log(err);
+  }
+);
