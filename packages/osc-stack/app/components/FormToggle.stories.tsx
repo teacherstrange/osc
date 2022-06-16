fb4b411c120b2b6b5f41b3055d6bbc70c74f4720
alym karim <akarim@openstudycollege.com>
2022-06-16 10:27:47 +0100

    fix: prettier formated formtoggle stories

--- packages/osc-stack/app/components/FormToggle.stories.tsx
+++ packages/osc-stack/app/components/FormToggle.stories.tsx
@@ -1,18 +1,18 @@
-import type { Meta, Story } from "@storybook/react";
-import FormToggle from "./FormToggle";
-import type { Props } from "./FormToggle";
+import type { Meta, Story } from '@storybook/react';
+import FormToggle from './FormToggle';
+import type { Props } from './FormToggle';
 
 export default {
-  title: "FormToggle",
-  component: FormToggle,
+    title: 'FormToggle',
+    component: FormToggle
 } as Meta;
 
 const Template: Story<Props> = (args) => {
-  return <FormToggle {...args}></FormToggle>;
+    return <FormToggle {...args}></FormToggle>;
 };
 
 export const Primary = Template.bind({});
 
 Primary.args = {
-  id: "storybook-form-toggle",
+    id: 'storybook-form-toggle'
 };
