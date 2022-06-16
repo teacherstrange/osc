fb1dcbae192873f9e3903b2bc465a4d5321f1fb6
alym karim <akarim@openstudycollege.com>
2022-06-16 10:21:48 +0100

    fix: prettier formated preview.js

--- packages/osc-stack/.storybook/preview.js
+++ packages/osc-stack/.storybook/preview.js
@@ -7,51 +7,51 @@
 // import "/app/components/nav/nav.css";
 // import "/app/components/pagination/pagination.css";
 
-import darkTheme from "../app/theme/darkTheme";
-import lightTheme from "../app/theme/lightTheme";
-import { ChakraProvider } from "@chakra-ui/react";
+import darkTheme from '../app/theme/darkTheme';
+import lightTheme from '../app/theme/lightTheme';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import React from "react";
-import { addDecorator } from "@storybook/react";
-import { MemoryRouter } from "react-router";
-import { CacheProvider } from "@emotion/react";
-import createCache from "@emotion/cache";
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 
 const cache = createCache({
-  key: "css",
-  prepend: true,
+    key: 'css',
+    prepend: true
 });
 
 addDecorator((story) => {
-  return (
-    <CacheProvider value={cache}>
-      <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
-    </CacheProvider>
-  );
+    return (
+        <CacheProvider value={cache}>
+            <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+        </CacheProvider>
+    );
 });
 
 export const parameters = {
-  chakra: {
-    theme: darkTheme,
-  },
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    chakra: {
+        theme: darkTheme
     },
-  },
-  backgrounds: {
-    default: "dark",
-    values: [
-      {
-        name: "dark",
-        value: "#272145",
-      },
-      {
-        name: "light",
-        value: "#fff",
-      },
-    ],
-  },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/
+        }
+    },
+    backgrounds: {
+        default: 'dark',
+        values: [
+            {
+                name: 'dark',
+                value: '#272145'
+            },
+            {
+                name: 'light',
+                value: '#fff'
+            }
+        ]
+    }
 };
