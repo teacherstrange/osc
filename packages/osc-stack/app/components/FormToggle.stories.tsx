af9a5816e071cdfdab965e0cfc7cff8cc80526b1
alym karim <akarim@openstudycollege.com>
2022-06-16 10:26:21 +0100

    fix: aded formtoggle story to build

---
+++ packages/osc-stack/app/components/FormToggle.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, Story } from "@storybook/react";
+import FormToggle from "./FormToggle";
+import type { Props } from "./FormToggle";
+
+export default {
+  title: "FormToggle",
+  component: FormToggle,
+} as Meta;
+
+const Template: Story<Props> = (args) => {
+  return <FormToggle {...args}></FormToggle>;
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  id: "storybook-form-toggle",
+};
