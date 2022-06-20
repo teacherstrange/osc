65b5d280196d64010f268934c91f43c68e5748a0
alym karim <akarim@openstudycollege.com>
2022-06-20 15:18:54 +0100

    feat: added gtm_tracking_id

--- packages/osc-stack/app/root.tsx
+++ packages/osc-stack/app/root.tsx
@@ -134,7 +134,7 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache: Em
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${googleTagManagerId}');`
+                })(window,document,'script','dataLayer',${googleTagManagerId});`
                     }}
                 ></script>
                 {serverStyleData.map(({ key, ids, css }) => (
