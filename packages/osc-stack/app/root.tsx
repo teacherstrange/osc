11f9c8d1d62a40d65b1147592356b8787ba763f9
alym karim <akarim@openstudycollege.com>
2022-06-20 15:02:03 +0100

    fix: amend changes

--- packages/osc-stack/app/root.tsx
+++ packages/osc-stack/app/root.tsx
@@ -17,7 +17,7 @@ import styles from 'app/styles/dest/main.css';
 import { getUser } from './session.server';
 import { checkConnectivity } from '~/utils/client/pwa-utils.client';
 import { PushNotification } from '~/utils/server/pwa-utils.server';
-import * as gtag from "~/utils/gtags.client";
+import * as gtag from '~/utils/gtags.client';
 
 import tailwindStylesheetUrl from './styles/tailwind.css';
 
@@ -71,7 +71,7 @@ export const loader: LoaderFunction = async ({ request }) => {
     return json<LoaderData>({
         user: await getUser(request),
         colorScheme: await getColorScheme(request),
-        gaTrackingId: process.env.NODE_ENV === "production" ? process.env.GA_TRACKING_ID : undefined
+        gaTrackingId: process.env.NODE_ENV === 'production' ? process.env.GA_TRACKING_ID : undefined
     });
 };
 interface DocumentProps {
@@ -116,10 +116,10 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache: Em
     const { gaTrackingId } = useLoaderData<LoaderData>();
 
     useEffect(() => {
-    if (gaTrackingId?.length) {
-      gtag.pageview(location.pathname, gaTrackingId);
-    }
-  }, [location, gaTrackingId]);
+        if (gaTrackingId?.length) {
+            gtag.pageview(location.pathname, gaTrackingId);
+        }
+    }, [location, gaTrackingId]);
     return (
         <html lang="en" className="h-full">
             <head>
@@ -134,28 +134,28 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache: Em
                 ))}
             </head>
             <body>
-                {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
-            />
-            <script
-              async
-              id="gtag-init"
-              dangerouslySetInnerHTML={{
-                __html: `
+                {process.env.NODE_ENV === 'development' || !gaTrackingId ? null : (
+                    <>
+                        <script
+                            async
+                            src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+                        />
+                        <script
+                            async
+                            id="gtag-init"
+                            dangerouslySetInnerHTML={{
+                                __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', '${gaTrackingId}', {
                   page_path: window.location.pathname,
                 });
-              `,
-              }}
-            />
-          </>
-        )}
+              `
+                            }}
+                        />
+                    </>
+                )}
                 {children}
                 <ScrollRestoration />
                 <Scripts />
