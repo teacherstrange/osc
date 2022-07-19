2a09c9764a835b6fd972cfa73c1fa2bb85287bac
alym karim <akarim@openstudycollege.com>
2022-07-19 13:09:46 +0100

    fix: disabled js linting in lint staged, testing linting in deploy steps

--- packages/header/dist/index.js
+++ packages/header/dist/index.js
@@ -5,9 +5,11 @@ Object.defineProperty(exports, '__esModule', { value: true });
 var React = require('react');
 var react = require('@chakra-ui/react');
 
-function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+function _interopDefaultLegacy(e) {
+    return e && typeof e === 'object' && 'default' in e ? e : { default: e };
+}
 
-var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+var React__default = /*#__PURE__*/ _interopDefaultLegacy(React);
 
 /******************************************************************************
 Copyright (c) Microsoft Corporation.
@@ -24,22 +26,24 @@ OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 PERFORMANCE OF THIS SOFTWARE.
 ***************************************************************************** */
 
-var __assign = function() {
-    __assign = Object.assign || function __assign(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-        }
-        return t;
-    };
+var __assign = function () {
+    __assign =
+        Object.assign ||
+        function __assign(t) {
+            for (var s, i = 1, n = arguments.length; i < n; i++) {
+                s = arguments[i];
+                for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+            }
+            return t;
+        };
     return __assign.apply(this, arguments);
 };
 
 function __rest(s, e) {
     var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+    for (var p in s)
+        if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === 'function')
         for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
             if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
                 t[p[i]] = s[p[i]];
@@ -48,20 +52,58 @@ function __rest(s, e) {
 }
 
 var Header = function (props) {
-    var className = props.className, other = __rest(props, ["className"]);
-    return (React__default["default"].createElement(react.Accordion, __assign({ className: "o-header ".concat(className) }, other),
-        React__default["default"].createElement(react.AccordionItem, null,
-            React__default["default"].createElement("h2", null,
-                React__default["default"].createElement(react.AccordionButton, null,
-                    React__default["default"].createElement(react.Box, { flex: "1", textAlign: "left" }, "changed title again"),
-                    React__default["default"].createElement(react.AccordionIcon, null))),
-            React__default["default"].createElement(react.AccordionPanel, { color: 'primary', bg: 'secondary', pb: 4 }, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")),
-        React__default["default"].createElement(react.AccordionItem, null,
-            React__default["default"].createElement("h2", null,
-                React__default["default"].createElement(react.AccordionButton, null,
-                    React__default["default"].createElement(react.Box, { flex: "1", textAlign: "left" }, "Section 2 title"),
-                    React__default["default"].createElement(react.AccordionIcon, null))),
-            React__default["default"].createElement(react.AccordionPanel, { pb: 4 }, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."))));
+    var className = props.className,
+        other = __rest(props, ['className']);
+    return React__default['default'].createElement(
+        react.Accordion,
+        __assign({ className: 'o-header '.concat(className) }, other),
+        React__default['default'].createElement(
+            react.AccordionItem,
+            null,
+            React__default['default'].createElement(
+                'h2',
+                null,
+                React__default['default'].createElement(
+                    react.AccordionButton,
+                    null,
+                    React__default['default'].createElement(
+                        react.Box,
+                        { flex: '1', textAlign: 'left' },
+                        'changed title again'
+                    ),
+                    React__default['default'].createElement(react.AccordionIcon, null)
+                )
+            ),
+            React__default['default'].createElement(
+                react.AccordionPanel,
+                { color: 'primary', bg: 'secondary', pb: 4 },
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+            )
+        ),
+        React__default['default'].createElement(
+            react.AccordionItem,
+            null,
+            React__default['default'].createElement(
+                'h2',
+                null,
+                React__default['default'].createElement(
+                    react.AccordionButton,
+                    null,
+                    React__default['default'].createElement(
+                        react.Box,
+                        { flex: '1', textAlign: 'left' },
+                        'Section 2 title'
+                    ),
+                    React__default['default'].createElement(react.AccordionIcon, null)
+                )
+            ),
+            React__default['default'].createElement(
+                react.AccordionPanel,
+                { pb: 4 },
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+            )
+        )
+    );
 };
 
 exports.Header = Header;
