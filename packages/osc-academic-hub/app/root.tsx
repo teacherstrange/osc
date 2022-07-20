d15291b7085740f87ec3c61ff0948905497731e1
alym karim <akarim@openstudycollege.com>
2022-07-20 13:18:48 +0100

    fix: added browerlist to remix-academic-hub package.json

--- packages/osc-academic-hub/app/root.tsx
+++ packages/osc-academic-hub/app/root.tsx
@@ -27,7 +27,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
     charset: 'utf-8',
-    title: 'Remix Notes',
+    title: 'OSC Academic Hub',
     viewport: 'width=device-width,initial-scale=1'
 });
 
