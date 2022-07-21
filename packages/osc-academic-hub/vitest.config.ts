c571a7863ddc7f2d891864f794a8fd91cfb4404c
alym karim <akarim@openstudycollege.com>
2022-07-21 15:08:53 +0100

    fix: css === false in vitest.config.js osc-ui

--- packages/osc-academic-hub/vitest.config.ts
+++ packages/osc-academic-hub/vitest.config.ts
@@ -1,14 +1,12 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import type { CSSOptions } from 'vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    css: true as CSSOptions,
     test: {
         globals: true,
         environment: 'happy-dom',
