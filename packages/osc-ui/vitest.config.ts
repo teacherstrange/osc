d1eb8758a4b54a1aeafe9659993010960276663f
alym karim <akarim@openstudycollege.com>
2022-07-21 13:29:55 +0100

    fix: updated vite version

--- packages/osc-ui/vitest.config.ts
+++ packages/osc-ui/vitest.config.ts
@@ -1,15 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import type { CSSOptions } from 'vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    css: false as CSSOptions,
     test: {
+        css: false,
         globals: true,
         environment: 'happy-dom',
         setupFiles: ['./test/setup-test-env.ts'],
