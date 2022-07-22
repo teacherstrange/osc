8b16c6f16ab79ba663ca511308c4c58c7ae4a8bc
alym karim <akarim@openstudycollege.com>
2022-07-22 11:12:04 +0100

    fix: created vitest import css plugn

--- packages/osc-ui/vitest.config.ts
+++ packages/osc-ui/vitest.config.ts
@@ -3,11 +3,60 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import libCss from 'vite-plugin-libcss';
+// import libCss from 'vite-plugin-libcss';
 import tsconfigPaths from 'vite-tsconfig-paths';
+const { resolve } = require('path');
+const fs = require('fs');
+
+export interface PluginOptions {
+    [x: string]: string;
+}
+
+let viteConfig;
+
+const importCss = (options: PluginOptions = {}): any => {
+    return {
+        name: 'lib-css',
+        apply: 'build',
+        enforce: 'post',
+
+        configResolved(resolvedConfig) {
+            viteConfig = resolvedConfig;
+        },
+
+        writeBundle(option, bundle) {
+            // if (!viteConfig.build || !viteConfig.build.lib) {
+            //     // only for lib build
+            //     console.warn('vite-plugin-libcss only works in lib mode.');
+            //     return;
+            // }
+            // if (option.format !== 'es') {
+            //     // only for es built
+            //     return;
+            // }
+            const files = Object.keys(bundle);
+            const cssFile = files.find((v) => v.endsWith('.css'));
+            if (!cssFile) {
+                return;
+            }
+            for (const file of files) {
+                if (!bundle[file].isEntry) {
+                    // only for entry
+                    continue;
+                }
+                const outDir = viteConfig.build.outDir || 'dist';
+                const filePath = resolve(viteConfig.root, outDir, file);
+                const data = fs.readFileSync(filePath, {
+                    encoding: 'utf8'
+                });
+                fs.writeFileSync(filePath, `import './${cssFile}';\n${data}`);
+            }
+        }
+    };
+};
 
 export default defineConfig({
-    plugins: [libCss(), react(), tsconfigPaths()],
+    plugins: [importCss(), react(), tsconfigPaths()],
     test: {
         globals: true,
         environment: 'happy-dom',
