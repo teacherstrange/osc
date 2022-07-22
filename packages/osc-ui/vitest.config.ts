c9bb0bb8507d3310b47fb969e834a0e3aa5df4d4
alym karim <akarim@openstudycollege.com>
2022-07-22 03:55:47 +0100

    fix: replace vitest watch with vitest run

--- packages/osc-ui/vitest.config.ts
+++ packages/osc-ui/vitest.config.ts
@@ -9,6 +9,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
     plugins: [libCss(), react(), tsconfigPaths()],
     test: {
+        reporters: ['verbose'],
         globals: true,
         environment: 'happy-dom',
         setupFiles: ['./test/setup-test-env.ts'],
