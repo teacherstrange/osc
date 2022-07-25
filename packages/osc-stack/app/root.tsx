130523a22a4789e686ba5312efa8364708d64219
alym karim <akarim@openstudycollege.com>
2022-07-25 12:57:51 +0100

    fix: added remix-pwa

--- packages/osc-stack/app/root.tsx
+++ packages/osc-stack/app/root.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect } from 'react';
+import { useLocation, useMatches } from '@remix-run/react';
 import type { HeadersFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { getColorScheme } from './cookie';
@@ -11,42 +13,92 @@ import { withEmotionCache } from '@emotion/react';
 import type { EmotionCache } from '@emotion/react';
 import { useEmotionCache } from './hooks/useEmotionCache';
 import DOMPurify from 'isomorphic-dompurify';
-
 import styles from 'app/styles/dest/main.css';
 import { getUser } from './session.server';
+import { checkConnectivity } from '~/utils/client/pwa-utils.client';
 
+let isMount = true;
 export const links: LinksFunction = () => {
-    return [{ rel: 'stylesheet', href: styles }];
+    return [
+        { rel: 'stylesheet', href: styles },
+        { rel: 'manifest', href: '/resources/manifest.webmanifest' },
+        { rel: 'apple-touch-icon', sizes: '57x57', href: '/icons/apple-icon-57x57.png' },
+        { rel: 'apple-touch-icon', sizes: '60x60', href: '/icons/apple-icon-60x60.png' },
+        { rel: 'apple-touch-icon', sizes: '72x72', href: '/icons/apple-icon-72x72.png' },
+        { rel: 'apple-touch-icon', sizes: '76x76', href: '/icons/apple-icon-76x76.png' },
+        { rel: 'apple-touch-icon', sizes: '114x114', href: '/icons/apple-icon-114x114.png' },
+        { rel: 'apple-touch-icon', sizes: '120x120', href: '/icons/apple-icon-120x120.png' },
+        { rel: 'apple-touch-icon', sizes: '144x144', href: '/icons/apple-icon-144x144.png' },
+        { rel: 'apple-touch-icon', sizes: '152x152', href: '/icons/apple-icon-152x152.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-icon-180x180.png' },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '192x192',
+            href: '/icons/android-icon-192x192.png'
+        },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icons/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' }
+    ];
 };
-
 export const meta: MetaFunction = () => ({
     charset: 'utf-8',
     title: 'Remix Notes',
     viewport: 'width=device-width,initial-scale=1'
 });
-
 type LoaderData = {
     user: Awaited<ReturnType<typeof getUser>>;
     colorScheme: string;
 };
-
 export const headers: HeadersFunction = () => ({
     'Accept-CH': 'Sec-CH-Prefers-Color-Scheme'
 });
-
 export const loader: LoaderFunction = async ({ request }) => {
     return json<LoaderData>({
         user: await getUser(request),
         colorScheme: await getColorScheme(request)
     });
 };
-
 interface DocumentProps {
     children: React.ReactNode;
 }
 
 const Document = withEmotionCache(({ children }: DocumentProps, emotionCache: EmotionCache) => {
     const serverStyleData = useEmotionCache(emotionCache);
+    let location = useLocation();
+    let matches = useMatches();
+
+    useEffect(() => {
+        let mounted = isMount;
+        isMount = false;
+        if ('serviceWorker' in navigator) {
+            if (navigator.serviceWorker.controller) {
+                navigator.serviceWorker.controller?.postMessage({
+                    type: 'REMIX_NAVIGATION',
+                    isMount: mounted,
+                    location,
+                    matches,
+                    manifest: window.__remixManifest
+                });
+            } else {
+                let listener = async () => {
+                    await navigator.serviceWorker.ready;
+                    navigator.serviceWorker.controller?.postMessage({
+                        type: 'REMIX_NAVIGATION',
+                        isMount: mounted,
+                        location,
+                        matches,
+                        manifest: window.__remixManifest
+                    });
+                };
+                navigator.serviceWorker.addEventListener('controllerchange', listener);
+                return () => {
+                    navigator.serviceWorker.removeEventListener('controllerchange', listener);
+                };
+            }
+        }
+    }, [location, matches]);
     return (
         <html lang="en" className="h-full">
             <head>
@@ -72,6 +124,20 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache: Em
 
 export default function App() {
     const { colorScheme } = useLoaderData();
+
+    const online = () => {
+        //..Do something for online state
+        console.log('Online');
+    };
+
+    const offline = () => {
+        //..Do something for offline state
+    };
+
+    useEffect(() => {
+        // The `console.log` method returns an object with a status of "success" if online and a pass message or a status of "bad" and a fail message if offline
+        checkConnectivity(online, offline).then((data) => console.log(data));
+    }, []);
     return (
         <Document>
             <ChakraProvider theme={colorScheme === 'light' ? lightTheme : darkTheme}>
