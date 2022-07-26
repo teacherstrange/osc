52dfb4ad2cdde9bc7306eacf1134b3517505ddfb
alym karim <akarim@openstudycollege.com>
2022-07-26 18:19:20 +0100

    fix: renamed remix notes to osc-stack

--- packages/osc-stack/app/root.tsx
+++ packages/osc-stack/app/root.tsx
@@ -54,7 +54,7 @@ export const links: LinksFunction = () => {
 };
 export const meta: MetaFunction = () => ({
     charset: 'utf-8',
-    title: 'Remix Notes',
+    title: 'OSC Stack',
     viewport: 'width=device-width,initial-scale=1'
 });
 type LoaderData = {
