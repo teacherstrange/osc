743cc73bd436e4c0368bc20839f79a94f74c4827
alym karim <akarim@openstudycollege.com>
2022-08-18 11:25:36 +0100

    fix: wait for deployrequest merged after creation

--- .pscale/cli-helper-scripts/ps-create-helper-functions-pr-branches.sh
+++ .pscale/cli-helper-scripts/ps-create-helper-functions-pr-branches.sh
@@ -254,5 +254,14 @@ function create-deployment {
         pscale deploy-request close "$DB_NAME" "$DEPLOY_REQUEST_NUMBER" --org "$ORG_NAME"
     else
         pscale deploy-request deploy "$DB_NAME" "$DEPLOY_REQUEST_NUMBER" --org "$ORG_NAME"
+
+        wait_for_deploy_request_merged 9 "$DB_NAME" "$DEPLOY_REQUEST_NUMBER" "$ORG_NAME" 60
+        if [ $? -ne 0 ]; then
+            echo "Error: wait-for-deploy-request-merged returned non-zero exit code"
+            echo "Check out the deploy request status at $deploy_request"
+            exit 5
+        else
+            echo "Check out the deploy request at $deploy_request"
+        fi
     fi
 }
