a940d0317b7d1c85c52f1f798422cdad1dcf57de
alym karim <akarim@openstudycollege.com>
2022-08-18 11:32:23 +0100

    fix: syntax error

--- .pscale/cli-helper-scripts/create-dr-to-main.sh
+++ .pscale/cli-helper-scripts/create-dr-to-main.sh
@@ -6,6 +6,5 @@
 . .pscale/cli-helper-scripts/ps-create-helper-functions-pr-branches.sh
 create-schema-change "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DDL_STATEMENTS"
 create-deploy-request "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
-wait-for-deploy-request-merged 9 "$DB_NAME" "$DEPLOY_REQUEST_NUMBER" "$ORG_NAME"
 
 . .pscale/cli-helper-scripts/merge-or-close-deploy-request.sh "$DB_NAME" "$BRANCH_NAME" "$DEPLOY_REQUEST_NUMBER" "$ORG_NAME"
