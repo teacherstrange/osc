a889e5c4c6b1e02ce9c7a074525b5f6bf81c23d8
alym karim <akarim@openstudycollege.com>
2022-08-19 16:58:26 +0100

    fix: addd untracked dockerfiles

---
+++ .pscale/cli-helper-scripts/create-and-merge-deployment-request.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. use-pscale-docker-image.sh
+. authenticate-ps.sh
+. wait-for-deploy-request-merged.sh
+. set-db-and-org-and-branch-name.sh
+. ps-create-helper-functions.sh
+
+# if no parameter was passed, display usage
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <deploy request id>"
+    exit 1
+fi
+
+# need deployment request number from create-deploy-request
+create-deploy-request "$DB_NAME" "$ORG_NAME" "$1"
+create-deployment "$DB_NAME" "$ORG_NAME" "$1"
