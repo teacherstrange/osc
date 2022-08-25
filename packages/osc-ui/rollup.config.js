a689b3bf461acf910a6c7b6d6cdb7f050794efea
alym karim <akarim@openstudycollege.com>
2022-08-25 13:26:44 +0100

    fix: bundle types with components

--- packages/osc-ui/rollup.config.js
+++ packages/osc-ui/rollup.config.js
@@ -1,23 +1,34 @@
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
+import del from 'rollup-plugin-delete';
 import { peerDependencies } from './package.json';
 
-export default {
-    input: ['src/index.tsx'],
-    output: [
-        {
-            dir: 'dist',
-            entryFileNames: '[name].js',
-            format: 'cjs',
-            exports: 'named'
-        }
-    ],
-    plugins: [
-        postcss({
-            sourceMap: false,
-            extract: true
-        }),
-        typescript({ check: false })
-    ],
-    external: Object.keys(peerDependencies)
-};
+export default [
+    {
+        input: ['src/index.tsx'],
+        output: [
+            {
+                dir: 'dist',
+                entryFileNames: '[name].js',
+                format: 'cjs',
+                exports: 'named'
+            }
+        ],
+        plugins: [
+            postcss({
+                sourceMap: false,
+                extract: true
+            }),
+            typescript({ check: false })
+        ],
+        external: Object.keys(peerDependencies)
+    },
+    {
+        // path to your declaration files root
+        input: 'dist/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        external: [/\.css$/],
+        plugins: [dts(), del({ hook: 'buildEnd', targets: './dist/components' })]
+    }
+];
