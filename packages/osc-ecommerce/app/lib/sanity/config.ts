3b2a9c7ceafcbdaaf0e0048c96eb286b0d43b8e4
Steven Bister <steve.bister@openstudycollege.com>
2022-09-30 12:46:27 +0100

    feat: updated files to ts

--- packages/osc-ecommerce/app/lib/sanity/config.ts
+++ packages/osc-ecommerce/app/lib/sanity/config.ts
@@ -1,4 +1,7 @@
 // Becuase this needs to work on both the server and the browser we need to check for the document being defined https://remix.run/docs/en/v1/guides/constraints#document-guard
+
+import invariant from 'tiny-invariant';
+
 // The document.env should be set in the root.tsx file so we can access it across all routes
 const SANITY_STUDIO_API_PROJECT_ID =
     typeof document !== 'undefined'
@@ -9,6 +12,9 @@ const SANITY_STUDIO_API_DATASET =
     typeof document !== 'undefined'
         ? document.env.SANITY_STUDIO_API_DATASET
         : process.env.SANITY_STUDIO_API_DATASET;
+
+invariant(SANITY_STUDIO_API_PROJECT_ID, `SANITY_STUDIO_API_PROJECT_ID must be set`);
+invariant(SANITY_STUDIO_API_DATASET, `SANITY_STUDIO_API_DATASET must be set`);
 
 export const config = {
     apiVersion: '2021-10-21', // see studio/contstants.js
