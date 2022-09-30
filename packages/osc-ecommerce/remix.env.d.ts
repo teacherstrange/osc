8360d600f0dc3842ef35bbad85fdd63878f83bc4
Steven Bister <steve.bister@openstudycollege.com>
2022-09-30 12:48:40 +0100

    feat: adds sanity settings to document object

--- packages/osc-ecommerce/remix.env.d.ts
+++ packages/osc-ecommerce/remix.env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
+
+interface DocumentEnv {
+    readonly SANITY_STUDIO_API_PROJECT_ID: string;
+    readonly SANITY_STUDIO_API_DATASET: string;
+}
+
+interface Document {
+    readonly env: DocumentEnv;
+}
