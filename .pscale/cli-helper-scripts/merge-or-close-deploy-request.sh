ce14ae3a34b85afc9e0f934afef5023366fbe41d
alym karim <akarim@openstudycollege.com>
2022-10-12 14:55:03 +0100

    fix: echo previous command exit codes

--- .pscale/cli-helper-scripts/merge-or-close-deploy-request.sh
+++ .pscale/cli-helper-scripts/merge-or-close-deploy-request.sh
@@ -5,6 +5,7 @@
 . .pscale/cli-helper-scripts/ps-create-helper-functions-pr-branches.sh
 create-deployment "$DB_NAME" "$BRANCH_NAME" "$DEPLOY_REQUEST_NUMBER" "$ORG_NAME"
     if [ $? -ne 0 ]; then
+        echo "$?"
         echo "Error: merging deployment failed"
         return 1;
     fi
