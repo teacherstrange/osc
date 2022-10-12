a6492fc40bf285a2a6c2bdce913deab218dc4d12
alym karim <akarim@openstudycollege.com>
2022-10-12 12:15:15 +0100

    fix: exit in current shell not subshell

--- .pscale/cli-helper-scripts/create-dr-to-main.sh
+++ .pscale/cli-helper-scripts/create-dr-to-main.sh
@@ -7,8 +7,6 @@
 create-schema-change "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DDL_STATEMENTS"
 create-deploy-request "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
 
-if [ $? -ne '0' ]; then 
-  exit 1
-fi 
+[ $? -ne '0' ] && exit 1
 
 . .pscale/cli-helper-scripts/merge-or-close-deploy-request.sh "$DB_NAME" "$BRANCH_NAME" "$DEPLOY_REQUEST_NUMBER" "$ORG_NAME"
