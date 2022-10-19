71548d0df0edd6eb29505f47ef657155e187b066
Steven Bister <steve.bister@openstudycollege.com>
2022-10-19 15:45:42 +0100

    feat: add cors header to allow studio seo pane to fetch site

--- packages/osc-ecommerce/app/entry.server.tsx
+++ packages/osc-ecommerce/app/entry.server.tsx
@@ -38,6 +38,30 @@ export default function handleRequest(
 
     responseHeaders.set('Content-Type', 'text/html');
 
+    // We're using the SEO Pane plugin in our Sanity studio to generate Yoast like feedback for the editors.
+    // Sanity needs to fetch the site so the plugin can check it for settings like canonical tags, title etc.
+    // To do this we need to set the Access-Control-Allow-Origin response header to the url of the studio so we don't get blocked by CORS.
+    // See the "Fetching the front-end" section: https://www.sanity.io/plugins/seo-pane
+
+    // Make sure we're setting the correct corsOrigin url depending if we're on developement, viewing a pr url or main
+    const requestUrl = new URL(request?.url);
+    const prefixRegex = /((pr-)\d+)/g; // This bit of regex finds any text that matches "pr-<n>" e.g. pr-123
+
+    let deployedUrl;
+
+    if (requestUrl.hostname.match(prefixRegex)) {
+        const urlPrefix = requestUrl.hostname.match(prefixRegex)?.join('');
+
+        deployedUrl = `https://${urlPrefix}-osc-studio.fly.dev`;
+    } else {
+        deployedUrl = `https://osc-studio.fly.dev`;
+    }
+
+    const corsOrigin =
+        process.env.NODE_ENV === 'development' ? `http://localhost:3333` : deployedUrl;
+
+    responseHeaders.set('Access-Control-Allow-Origin', corsOrigin);
+
     return new Response(`<!DOCTYPE html>${markup}`, {
         status: responseStatusCode,
         headers: responseHeaders
