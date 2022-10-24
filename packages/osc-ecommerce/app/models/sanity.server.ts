4cb735633d074ba84efb84d95519bde04eda5447
Steven Bister <steve.bister@openstudycollege.com>
2022-10-24 10:13:50 +0100

    feat: adds function to create the url pathname

--- packages/osc-ecommerce/app/models/sanity.server.ts
+++ packages/osc-ecommerce/app/models/sanity.server.ts
@@ -1,11 +1,46 @@
 import type { Params } from 'react-router-dom';
-import type { SanityPage, SanitySiteSetting } from '~/types/sanity';
+import type { SanityPage, SanitySiteSetting, SanityRedirect } from '~/types/sanity';
 import { getClient } from '~/lib/sanity/getClient.server';
 
 /**
  * Exclude items that contain "drafts" in the _id
  */
 export const excludeDrafts = (item: { _id: string }) => !item._id.includes('drafts');
+
+/**
+ * Create the url pathname so we can handle subfolders
+ */
+interface BuildUrlArgs {
+    type: SanityRedirect['destination']['_type'];
+    url: URL;
+    slug: SanityRedirect['destination']['slug'];
+}
+
+export const buildUrlPath = ({ type, url, slug }: BuildUrlArgs) => {
+    switch (type) {
+        case 'home':
+            url.pathname = `/`;
+            break;
+
+        case 'post':
+            url.pathname = `blog/${slug}`;
+            break;
+
+        case 'collection':
+            url.pathname = `collections/${slug}`;
+            break;
+
+        case 'product':
+            url.pathname = `products/${slug}`;
+            break;
+
+        default:
+            url.pathname = slug;
+            break;
+    }
+
+    return url.pathname;
+};
 
 interface Args {
     request: Request;
@@ -52,3 +87,25 @@ export async function getSettingsData({ query }: { query: string }) {
         console.error(err);
     }
 }
+
+// export async function shouldRedirect(request: Request) {
+//     if (!request) throw new Error('Request must be passed');
+
+//     try {
+//         const url = new URL(request.url);
+//         const getRedirect = await getClient().fetch(REDIRECT, { slug: url.pathname });
+//         const redirectObject = getRedirect.filter((item: SanityRedirect) => excludeDrafts(item))[0];
+
+//         if (!redirectObject) return;
+
+//         const destination = buildUrlPath({
+//             type: redirectObject.destination._type,
+//             url,
+//             slug: redirectObject.destination.slug
+//         });
+
+//         return redirect(destination, redirectObject.statusCode);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
