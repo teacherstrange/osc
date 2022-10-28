a15b5c723079b02e63b508f4077fcae28f18fad0
Steven Bister <steve.bister@openstudycollege.com>
2022-10-28 10:22:57 +0100

    test: extends the expect function to check array for some items

--- packages/osc-ecommerce/__test__/setup-test-env.ts
+++ packages/osc-ecommerce/__test__/setup-test-env.ts
@@ -2,3 +2,22 @@ import { installGlobals } from '@remix-run/node';
 import '@testing-library/jest-dom/extend-expect';
 
 installGlobals();
+
+expect.extend({
+    arrayIncludesSome: (received: any[], expected: any[]) => {
+        const someExist = expected.some((value: any) => {
+            return received.includes(value);
+        });
+
+        if (!someExist) {
+            return {
+                message: () => `expected ${received} to include ${expected}`,
+                pass: false
+            };
+        }
+
+        return {
+            pass: true
+        };
+    }
+});
