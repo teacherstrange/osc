b8b905cf56c37b5885666d5213a2b54440d70a29
alym karim <akarim@openstudycollege.com>
2022-10-31 13:36:16 +0000

    fix: small change to see if checks appear

--- packages/osc-ecommerce/app/root.tsx
+++ packages/osc-ecommerce/app/root.tsx
@@ -217,6 +217,7 @@ export default function App() {
                     })}`
                 }}
             />
+            <h1>test</h1>
             <ChakraProvider theme={colorScheme === 'light' ? lightTheme : darkTheme}>
                 <Outlet />
             </ChakraProvider>
