010feb6777f74699c5bb1d487054f51e57dc000c
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2022-11-01 15:58:48 +0000

    feat: add css for conditional hover and re-work logic for rendering a link

--- packages/osc-ui/src/components/Breadcrumb/Breadcrumb.tsx
+++ packages/osc-ui/src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import type { FC } from 'react';
+import './breadcrumb.css';
+
 import {
     Breadcrumb as ChakraBreadcrumb,
     BreadcrumbItem as ChakraBreadcrumbItem,
@@ -21,19 +23,35 @@ export interface Props {
 export const Breadcrumb: FC<Props> = (props: Props) => {
     const { className, matches, separator } = props;
 
+    const shouldRenderALinkOrNot = (matches, match, idx) => {
+        let breadCrumb;
+        if (matches.length === idx + 1) {
+            // If it is the current page then don't add Link, and set to 'isCurrentPage'
+            breadCrumb = (
+                <ChakraBreadcrumbItem key={idx} isCurrentPage className="c-breadcrumb__item">
+                    <ChakraBreadcrumbLink>{match.title}</ChakraBreadcrumbLink>
+                </ChakraBreadcrumbItem>
+            );
+        } else {
+            breadCrumb = (
+                <ChakraBreadcrumbItem key={idx} className="c-breadcrumb__item">
+                    <ChakraBreadcrumbLink as={Link} to={match.pathname}>
+                        {match.title}
+                    </ChakraBreadcrumbLink>
+                </ChakraBreadcrumbItem>
+            );
+        }
+        return breadCrumb;
+    };
+
     return (
-        <ChakraBreadcrumb className={className ? className : ''} separator={separator}>
+        <ChakraBreadcrumb
+            className={`c-breadcrumb ${className ? className : ''}`}
+            separator={separator}
+        >
             {matches.map((match, idx) => {
-                return (
-                    <ChakraBreadcrumbItem
-                        key={idx}
-                        isCurrentPage={matches.length === idx + 1 ? true : false}
-                    >
-                        <ChakraBreadcrumbLink as={Link} to={match.pathname}>
-                            {match.title}
-                        </ChakraBreadcrumbLink>
-                    </ChakraBreadcrumbItem>
-                );
+                let chakraBreadCrumb = shouldRenderALinkOrNot(matches, match, idx);
+                return chakraBreadCrumb;
             })}
         </ChakraBreadcrumb>
     );
