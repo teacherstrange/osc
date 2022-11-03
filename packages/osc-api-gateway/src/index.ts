693f1d7a350e30526d73aef78d11c38421b079b2
Tom Pearce <thomas.pearce@openstudycollege.com>
2022-11-03 11:13:26 +0000

    feat: handling JWT auth header (WIP)

--- packages/osc-api-gateway/src/index.ts
+++ packages/osc-api-gateway/src/index.ts
@@ -1,10 +1,20 @@
 import { ApolloServer } from 'apollo-server-express';
-import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import * as dotenv from 'dotenv';
 import express from 'express';
+import { expressjwt } from 'express-jwt';
 
 dotenv.config();
 const app = express();
+
+app.use(
+    expressjwt({
+        secret: process.env.JWT_SECRET!,
+        audience: process.env.JWT_AUDIENCE!,
+        algorithms: ['HS256'],
+        credentialsRequired: false
+    })
+);
 
 // If we're in local development we're going to bypass the managed federation and use Introspect and Compose
 // We do not want to provide a routing url to apollo studio pointing at our local as it's not really built for it
@@ -13,12 +23,39 @@ const gateway =
         ? new ApolloGateway({
               supergraphSdl: new IntrospectAndCompose({
                   subgraphs: [{ name: 'OSC-Auth', url: process.env.AUTH_API_URL }]
-              })
+              }),
+              buildService({ name, url }) {
+                  return new RemoteGraphQLDataSource({
+                      url,
+                      willSendRequest({ request, context }) {
+                          request?.http?.headers.set(
+                              'user',
+                              context.user ? JSON.stringify(context.user) : ''
+                          );
+                      }
+                  });
+              }
           })
-        : new ApolloGateway();
+        : new ApolloGateway({
+              buildService({ name, url }) {
+                  return new RemoteGraphQLDataSource({
+                      url,
+                      willSendRequest({ request, context }) {
+                          request?.http?.headers.set(
+                              'user',
+                              context.user ? JSON.stringify(context.user) : ''
+                          );
+                      }
+                  });
+              }
+          });
 
 const server = new ApolloServer({
-    gateway
+    gateway,
+    context: ({ req }) => {
+        // const user = req.auth.user || null;
+        // return { user };
+    }
 });
 
 async function startServer(server: ApolloServer) {
