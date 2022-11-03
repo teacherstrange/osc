25eacbdc0724fc25ee26507328a5f03c94104b0b
Steven Bister <steve.bister@openstudycollege.com>
2022-11-03 12:15:24 +0000

    feat: adds content component to modules

--- packages/osc-ecommerce/app/components/Module.tsx
+++ packages/osc-ecommerce/app/components/Module.tsx
@@ -1,8 +1,40 @@
-import type { module, trustpilotModule, carouselModule } from '~/types/sanity';
-import { Trustpilot } from 'osc-ui';
-import { Carousel } from 'osc-ui';
+import type { LinkDescriptor } from '@remix-run/node';
+import { Carousel, Content, Trustpilot } from 'osc-ui';
+import contentStyles from 'osc-ui/dist/src-components-Content-content.css';
+import type {
+    carouselModule,
+    contentModule,
+    module,
+    SanityPage,
+    trustpilotModule
+} from '~/types/sanity';
 
-export default function Module({ module }: { module: module }) {
+// So we can dynamically add the styles of each component into remix we need to create an array of stylesheet objects.
+// We will then call this function in each of the `route` files where we have a dynamicLinks.
+// It's a bit boiler plate-y but is currently the only way to dynamically insert styles.
+export const getComponentStyles = (data: SanityPage) => {
+    const styles: LinkDescriptor[] = [];
+
+    if (!data?.modules) return [];
+
+    for (const module of data?.modules) {
+        switch (module._type) {
+            case 'module.content':
+                styles.push({ rel: 'stylesheet', href: contentStyles });
+                break;
+        }
+    }
+
+    return styles;
+};
+
+interface Props {
+    module: module;
+}
+
+export default function Module(props: Props) {
+    const { module } = props;
+
     switch (module._type) {
         case 'module.trustpilot':
             const moduleTrustpilot = module as trustpilotModule;
@@ -31,6 +63,21 @@ export default function Module({ module }: { module: module }) {
                     startIndex={moduleCarousel.startIndex} // fine
                 ></Carousel>
             );
+
+        case 'module.content':
+            const moduleContent = module as contentModule;
+
+            return moduleContent.body ? (
+                <Content
+                    align={moduleContent.horizontalAlignment}
+                    backgroundColor={moduleContent.backgroundColor}
+                    marginBottom={moduleContent.marginBottom}
+                    paddingBottom={moduleContent.paddingBottom}
+                    paddingTop={moduleContent.paddingTop}
+                    textColor={moduleContent.textColor}
+                    value={moduleContent.body}
+                />
+            ) : null;
 
         default:
             return null;
