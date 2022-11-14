6acaeae05e37d873a3b9c3610e550ea21ef3ae36
Steven Bister <steve.bister@openstudycollege.com>
2022-11-14 12:47:33 +0000

    refactor(components): replace FormToggle with ThemeSwitcher

---
+++ packages/osc-academic-hub/app/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,30 @@
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { Switch } from 'osc-ui';
+import type { FC } from 'react';
+
+export interface Props {
+    label: string;
+    onToggle: (isChecked: boolean) => void;
+    isChecked: boolean;
+}
+
+export const ThemeSwitcher: FC<Props> = (props) => {
+    const { label, onToggle, isChecked } = props;
+
+    return (
+        <>
+            <label htmlFor="color-mode-toggle" className="sr-only">
+                {label}
+            </label>
+            <MoonIcon />
+
+            <Switch
+                checked={isChecked}
+                onCheckedChange={(isChecked) => onToggle(isChecked)}
+                id="color-mode-toggle"
+            />
+
+            <SunIcon />
+        </>
+    );
+};
