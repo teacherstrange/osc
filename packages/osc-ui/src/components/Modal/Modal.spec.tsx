30b23919483c225f0ceff54d9e44c758c63239d7
Steven Bister <steve.bister@openstudycollege.com>
2022-11-14 14:08:00 +0000

    test(components): updated modal import in test file

--- packages/osc-ui/src/components/Modal/Modal.spec.tsx
+++ packages/osc-ui/src/components/Modal/Modal.spec.tsx
@@ -6,7 +6,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import type { Props } from './Modal';
-import { OSCModal as Modal } from './Modal';
+import { Modal } from './Modal';
 
 describe('Modal component', () => {
     const setup = ({
