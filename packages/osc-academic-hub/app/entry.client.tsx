a6baa578ab9382a561bf48ac29e9e420911367dd
Steven Bister <steve.bister@openstudycollege.com>
2022-11-14 12:50:31 +0000

    feat(sites): remove emotion cache

--- packages/osc-academic-hub/app/entry.client.tsx
+++ packages/osc-academic-hub/app/entry.client.tsx
@@ -1,36 +1,8 @@
 // entry.client.tsx
-import React, { useState } from 'react';
-import { hydrate } from 'react-dom';
-import { CacheProvider } from '@emotion/react';
 import { RemixBrowser } from '@remix-run/react';
+import { hydrate } from 'react-dom';
 
-import { ClientStyleContext } from './utils/context';
-import createEmotionCache from './createEmotionCache';
-
-interface ClientCacheProviderProps {
-    children: React.ReactNode;
-}
-
-function ClientCacheProvider({ children }: ClientCacheProviderProps) {
-    const [cache, setCache] = useState(createEmotionCache());
-
-    function reset() {
-        setCache(createEmotionCache());
-    }
-
-    return (
-        <ClientStyleContext.Provider value={{ reset }}>
-            <CacheProvider value={cache}>{children}</CacheProvider>
-        </ClientStyleContext.Provider>
-    );
-}
-
-hydrate(
-    <ClientCacheProvider>
-        <RemixBrowser />
-    </ClientCacheProvider>,
-    document
-);
+hydrate(<RemixBrowser />, document);
 
 if ('serviceWorker' in navigator) {
     // Use the window load event to keep the page load performant
