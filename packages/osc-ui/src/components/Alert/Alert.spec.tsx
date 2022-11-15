5f43d916b534dbbcb5cc57285284b3a35f75e997
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2022-11-15 14:53:16 +0000

    test: add tests

---
+++ packages/osc-ui/src/components/Alert/Alert.spec.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+
+import { Alert, AlertDescription, AlertTitle } from './Alert';
+
+describe('Alert component', () => {
+    test('should render the correct text/classes for the title and description', async () => {
+        render(
+            <Alert status="info">
+                <AlertTitle title="OSC is going live on August 30th" />
+                <AlertDescription description="Make sure you are ready!" />
+            </Alert>
+        );
+        const title = await screen.findByText('OSC is going live on August 30th');
+        const description = await screen.findByText('Make sure you are ready!');
+
+        expect(title).toHaveClass('c-alert__title');
+        expect(title).toHaveTextContent('OSC is going live on August 30th');
+        expect(description).toHaveClass('c-alert__description');
+        expect(description).toHaveTextContent('Make sure you are ready!');
+    });
+    test('should render an SVG for the icon as default', () => {
+        const { container } = render(
+            <Alert status="warning">
+                <AlertTitle title="OSC is going live on August 30th" />
+                <AlertDescription description="Make sure you are ready!" />
+            </Alert>
+        );
+
+        const icon = container.querySelector('svg');
+        expect(icon.nodeName).toBe('SVG');
+    });
+    test('should not render an SVG if icon prop is passed in as "false"', () => {
+        const { container } = render(
+            <Alert displayIcon={false} status="success">
+                <AlertTitle title="OSC is going live on August 30th" />
+                <AlertDescription description="Make sure you are ready!" />
+            </Alert>
+        );
+        const icon = container.querySelector('svg');
+        expect(icon).toBeNull();
+    });
+
+    // TODO - Reintroduce test when button component is added
+    // test('should render a custom component', () => {
+    //     render(
+    //         <Alert displayIcon={false} status="error">
+    //             <AlertTitle title="OSC is going live on August 30th" />
+    //             <AlertDescription description="Make sure you are ready!" />
+    //             <Cross2Icon />
+    //         </Alert>
+    //     );
+
+    //     const closeButton = screen.getByLabelText('Close');
+    //     expect(closeButton).toBeInTheDocument();
+    // });
+});
