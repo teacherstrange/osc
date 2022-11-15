9cb762c77c93cb019c357ce9363edd84eb1faf3f
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2022-11-15 15:53:58 +0000

    test: add tests

---
+++ packages/osc-ui/src/components/CountdownClock/CountdownClock.spec.tsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import { CountdownClockContainer } from './CountdownClockContainer';
+import { act, screen, render } from '@testing-library/react';
+import type { Props } from './CountdownClockContainer';
+import { ClockIcon } from '@radix-ui/react-icons';
+
+const DAY = 24 * 60 * 60 * 1000;
+const HOUR = 1000 * 60 * 60;
+const MINUTE = 60 * 1000;
+const SECOND = 1000;
+const DAYS = 'd';
+const HOURS = 'h';
+const MINUTES = 'm';
+const SECONDS = 's';
+
+describe('Countdown clock component', () => {
+    const setup = ({ classNames, endDate, icon, name, titles }: Props) =>
+        render(
+            <CountdownClockContainer
+                classNames={classNames}
+                endDate={endDate}
+                icon={icon}
+                name={name}
+                titles={titles}
+            />
+        );
+    const setDate = (length) => new Date().getTime() + length;
+    const setPastDate = (length) => new Date().getTime() - length;
+
+    beforeEach(() => {
+        vi.useFakeTimers();
+    });
+    afterEach(() => {
+        vi.useRealTimers();
+    });
+
+    test('should render a countdown timer', () => {
+        const date = setDate(SECOND);
+        const spy = vi.spyOn(global, 'setInterval');
+
+        render(
+            <CountdownClockContainer
+                endDate={date}
+                titles={{ days: 'd', hours: 'h', minutes: 'm', seconds: 's' }}
+            />
+        );
+
+        act(() => {
+            vi.runOnlyPendingTimers();
+            vi.runOnlyPendingTimers();
+        });
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith(expect.any(Function), 1000);
+    });
+
+    test('should render a countdown timer when the date is in the future', () => {
+        const date = setDate(DAY);
+        setup({
+            endDate: date,
+            titles: { days: 'd', hours: 'h', minutes: 'm', seconds: 's' }
+        });
+
+        const result = screen.getByRole('timer');
+
+        expect(result).toBeInTheDocument();
+    });
+
+    test('should not render a countdown timer when the date is in the past', () => {
+        const date = setPastDate(DAY);
+
+        setup({ endDate: date, titles: { days: 'd', hours: 'h', minutes: 'm', seconds: 's' } });
+
+        expect(screen.queryByRole('timer')).not.toBeInTheDocument();
+    });
+
+    test('should call setInterval and run every second when a valid future date is set', () => {
+        const date = setDate(DAY);
+        const intervalSpy = vi.spyOn(global, 'setInterval');
+        setup({ endDate: date, titles: { days: 'd', hours: 'h', minutes: 'm', seconds: 's' } });
+
+        expect(intervalSpy).toHaveBeenCalledTimes(1);
+        expect(intervalSpy).toHaveBeenCalledWith(expect.any(Function), 1000);
+    });
+
+    test('should update the time every second', () => {
+        const date = setDate(DAY);
+
+        setup({
+            endDate: date,
+            titles: { days: DAYS, hours: HOURS, minutes: MINUTES, seconds: SECONDS }
+        });
+        const daysOnFirstRender = screen.getByText('d', { exact: true }).previousSibling;
+
+        act(() => {
+            vi.runOnlyPendingTimers();
+        });
+        const daysAfterOneSecond = screen.queryByText(DAYS, { exact: true });
+        const hoursAfterOneSecond = screen.queryByText(HOURS, { exact: true }).previousSibling;
+        const minutesAfterOneSecond = screen.queryByText(MINUTES, { exact: true }).previousSibling;
+        const secondsAfterOneSecond = screen.queryByText(SECONDS, { exact: true }).previousSibling;
+
+        expect(daysOnFirstRender).toHaveTextContent('01');
+        expect(daysAfterOneSecond).not.toBeInTheDocument();
+        expect(hoursAfterOneSecond).toHaveTextContent('23');
+        expect(minutesAfterOneSecond).toHaveTextContent('59');
+        expect(secondsAfterOneSecond).toHaveTextContent('59');
+    });
+
+    test('should only render minutes and seconds if timer is less than 1 hour', () => {
+        const date = setDate(MINUTE);
+
+        setup({
+            endDate: date,
+            titles: { days: DAYS, hours: HOURS, minutes: MINUTES, seconds: SECONDS }
+        });
+
+        expect(screen.queryByText(DAYS, { exact: true })).not.toBeInTheDocument();
+        expect(screen.queryByText(HOURS, { exact: true })).not.toBeInTheDocument();
+        expect(screen.getByText(MINUTES, { exact: true })).toBeInTheDocument();
+        expect(screen.getByText(SECONDS, { exact: true })).toBeInTheDocument();
+    });
+    test('should only render minutes, seconds and hours if timer is less than 1 day', () => {
+        const date = setDate(HOUR * 2);
+
+        setup({
+            endDate: date,
+            titles: { days: DAYS, hours: HOURS, minutes: MINUTES, seconds: SECONDS }
+        });
+
+        expect(screen.queryByText(DAYS, { exact: true })).not.toBeInTheDocument();
+        expect(screen.getByText(HOURS, { exact: true })).toBeInTheDocument();
+        expect(screen.getByText(MINUTES, { exact: true })).toBeInTheDocument();
+        expect(screen.getByText(SECONDS, { exact: true })).toBeInTheDocument();
+    });
+
+    test('should render an icon when passed in as a prop', () => {
+        const date = setDate(HOUR);
+        const Icon = ClockIcon;
+
+        const { container } = setup({
+            endDate: date,
+            icon: <Icon />,
+            titles: { days: DAYS, hours: HOURS, minutes: MINUTES, seconds: SECONDS }
+        });
+
+        expect(container.querySelectorAll('svg').length).toBe(1);
+    });
+
+    test('should render a name for the timer when passed in as a prop', () => {
+        const date = setDate(HOUR);
+        const title = 'Time is running out!';
+
+        setup({
+            endDate: date,
+            name: title,
+            titles: { days: DAYS, hours: HOURS, minutes: MINUTES, seconds: SECONDS }
+        });
+
+        expect(screen.getByText(title)).toBeInTheDocument();
+        expect(screen.getByText(title)).toHaveTextContent(title);
+    });
+});
