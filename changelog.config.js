5ab22de73c31225ae1e80d18fab6355369c1b7eb
Simon Shahriveri <si@hi-im-si.com>
2022-11-15 09:38:44 +0000

    fix(gh-actions) fixing issues with the release workflow

---
+++ changelog.config.js
@@ -0,0 +1,15 @@
+'use strict'
+const config = require('conventional-changelog-conventionalcommits');
+
+module.exports = config({
+    "types": [
+        { type: 'feat', section: '✨ Features' },
+        { type: 'fix', section: '🐛 Bugs' },
+        { type: 'ci', section: '⚙️ CI/CD Updates' },
+        { type: 'docs', section: '📝 Documentation' },
+        { type: 'perf', section: '⚡️ Performance' },
+        { type: 'test', section: '🧪 Tests' },
+        { type: 'refactor', section: '♻️ Refactors' },
+        { type: 'chore', section: '📦 General Housekeeping / Package Updates' },
+    ]
+})
