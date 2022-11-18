42a409d6c23e85d9dd6a8fb43b5a488679e56172
Steven Bister <steve.bister@openstudycollege.com>
2022-11-18 11:26:58 +0000

    test(accordion): add content test to accordion

--- packages/osc-ui/src/components/Accordion/Accordion.spec.tsx
+++ packages/osc-ui/src/components/Accordion/Accordion.spec.tsx
@@ -3,7 +3,11 @@
  */
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { Content } from '../Content/Content';
+import { textContent } from '../Content/textContent';
 import { Accordion, AccordionHeader, AccordionItem, AccordionPanel } from './Accordion';
 
 test('has the correct heading level', () => {
@@ -61,4 +65,29 @@ test('extends the classname on each component', () => {
     expect(document.querySelector('.item-classname')).toBeInTheDocument();
     expect(document.querySelector('.heading-classname')).toBeInTheDocument();
     expect(document.querySelector('.panel-classname')).toBeInTheDocument();
+});
+
+test('accepts content component as child', async () => {
+    const user = userEvent.setup();
+
+    render(
+        <MemoryRouter>
+            <Accordion type="single" className="accordion-classname">
+                <AccordionItem value="0" className="item-classname">
+                    <AccordionHeader className="heading-classname">Heading level 2</AccordionHeader>
+                    <AccordionPanel className="panel-classname">
+                        <Content value={textContent} align="left" />
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+        </MemoryRouter>
+    );
+
+    await user.click(screen.getByRole('button', { name: 'Heading level 2' }));
+
+    const region = screen.getByRole('region');
+    const content = document.querySelector('.c-content');
+
+    expect(region).toHaveAttribute('data-state', 'open');
+    expect(content).toBeVisible();
 });
