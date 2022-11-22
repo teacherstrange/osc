12fcfe3461ab6d8770240617bb10b86d773eefaf
Steven Bister <steve.bister@openstudycollege.com>
2022-11-22 12:03:09 +0000

    test(academic-hub): adds smoke test for the skiplink

--- packages/osc-academic-hub/e2e/smoke.spec.ts
+++ packages/osc-academic-hub/e2e/smoke.spec.ts
@@ -10,3 +10,16 @@ test.skip('confirm that the user is logged in', async ({ page }) => {
 
     await expect(h1).toBeVisible();
 });
+
+test('skips focus into the main content', async ({ page }) => {
+    await page.goto('/');
+
+    const skipLink = page.getByRole('link', { name: 'Skip to main content' });
+    const main = page.getByRole('main');
+
+    await skipLink.focus();
+    await expect(skipLink).toBeFocused();
+
+    await page.keyboard.press('Enter');
+    await expect(main).toBeFocused();
+});
