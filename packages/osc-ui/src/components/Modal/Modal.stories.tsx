ea58fa447e6b29de44c927961d70f250869c5960
Steven Bister <steve.bister@openstudycollege.com>
2022-11-22 12:28:09 +0000

    docs: adds types and required values to storybook table

--- packages/osc-ui/src/components/Modal/Modal.stories.tsx
+++ packages/osc-ui/src/components/Modal/Modal.stories.tsx
@@ -21,38 +21,113 @@ export default {
             }
         },
         disableOutsideClick: {
-            description: 'Sets whether the user can click outside of the modal to close it'
+            description: 'Sets whether the user can click outside of the modal to close it',
+            table: {
+                type: {
+                    summary: 'boolean'
+                }
+            }
         },
         hideFooterCloseButton: {
-            description: 'Sets whether or not to prevent rendering of the close button in the footer'
+            description:
+                'Sets whether or not to prevent rendering of the close button in the footer',
+            type: { name: 'boolean', required: true },
+            table: {
+                type: {
+                    summary: 'boolean'
+                }
+            }
         },
         hideHeaderCloseButton: {
-            description: 'Sets whether or not to prevent rendering of the close button in the header'
+            description:
+                'Sets whether or not to prevent rendering of the close button in the header',
+            type: { name: 'boolean', required: true },
+            table: {
+                type: {
+                    summary: 'boolean'
+                }
+            }
         },
         ModalButtonText: {
-            description: 'Sets the text of the button that opens the modal'
+            description: 'Sets the text of the button that opens the modal',
+            type: { name: 'string', required: true },
+            table: {
+                type: {
+                    summary: 'boolean'
+                }
+            }
         },
         modalDescription: {
-            description: 'An accessible description to be announced when the dialog is opened'
+            description: 'An accessible description to be announced when the dialog is opened',
+            type: { name: 'string', required: true },
+            table: {
+                type: {
+                    summary: 'string'
+                }
+            }
+        },
+        onOpenChange: {
+            description: 'Event handler called when the open state of the dialog changes.',
+            table: {
+                type: {
+                    summary: '(open: boolean) => void'
+                }
+            }
         },
         onClick: {
-            description: 'The function that is passed to the primary action button'
+            description: 'The function that is passed to the primary action button',
+            type: { name: 'function', required: true },
+            table: {
+                type: {
+                    summary: '() => void'
+                }
+            }
         },
         overlayColour: {
-            description: 'Colour of the overlay background'
+            description: 'Colour of the overlay background',
+            type: { name: 'string', required: true },
+            table: {
+                type: {
+                    summary: 'string'
+                }
+            }
         },
         primaryActionButton: {
-            description: 'Sets whether the primary action button is visible'
+            description: 'Sets whether the primary action button is visible',
+            type: { name: 'boolean', required: true },
+            table: {
+                type: {
+                    summary: 'boolean'
+                }
+            }
         },
         primaryActionButtonText: {
-            description: 'Sets the text of the primary action button'
+            description: 'Sets the text of the primary action button',
+            type: { name: 'string', required: true },
+            table: {
+                type: {
+                    summary: 'string'
+                }
+            }
         },
         size: {
             description: 'Sets the size of the modal',
-            control: { type: 'select', options: ['xs', 'sm', 'md', 'lg', 'xl', 'full'] }
+            control: { type: 'select', options: ['xs', 'sm', 'md', 'lg', 'xl', 'full'] },
+            type: { name: 'string', required: true },
+            table: {
+                type: {
+                    summary: 'string'
+                }
+            }
         },
         title: {
-            description: 'Modal heading'
+            description: 'Modal heading',
+            type: { name: 'string', required: true },
+            table: {
+                type: {
+                    summary: 'string'
+                }
+            }
         }
     }
 } as Meta;
