359e81a9da0e398cc0e5aeaff63a1ead342a9b57
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2022-11-22 11:34:44 +0000

    test: add more  tests and add resize-observer polyfill

--- packages/osc-ui/src/components/Popover/Popover.spec.tsx
+++ packages/osc-ui/src/components/Popover/Popover.spec.tsx
@@ -1,24 +1,63 @@
-import { act, render, screen } from '@testing-library/react';
+/**
+ * @vitest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import React from 'react';
-import { Popover, PopoverContent, PopoverTrigger } from './Popover';
+import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from './Popover';
 
-test('should render popover content when trigger is clicked and close when trigger is clicked again', () => {
+global.ResizeObserver = require('resize-observer-polyfill');
+
+test('should render popover content when trigger is clicked and close when trigger is clicked again', async () => {
+    const user = userEvent.setup();
     render(
         <Popover>
             <PopoverTrigger>Click to toggle</PopoverTrigger>
             <PopoverContent>Test Content</PopoverContent>
         </Popover>
     );
-
-    act(() => {
-        screen.getByRole('button').click();
-    });
+    await user.click(screen.getByRole('button', { name: /Click to toggle/i }));
     expect(screen.getByRole('dialog')).toBeInTheDocument();
     expect(screen.getByRole('dialog')).toHaveTextContent('Test Content');
 
-    act(() => {
-        screen.getByRole('button').click();
-    });
+    await user.click(screen.getByRole('button', { name: /Click to toggle/i }));
     expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+});
+
+test('should render popover content when trigger is clicked and close when parent content is clicked', async () => {
+    const user = userEvent.setup();
+    render(
+        <div>
+            Parent content
+            <Popover>
+                <PopoverTrigger>Click to toggle</PopoverTrigger>
+                <PopoverContent>Test Content</PopoverContent>
+            </Popover>
+        </div>
+    );
+    await user.click(screen.getByRole('button', { name: /Click to toggle/i }));
+    expect(screen.getByRole('dialog')).toBeInTheDocument();
+
+    await user.click(screen.getByText(/Parent content/i));
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+});
+
+test('should render <PopoverClose> when content is opened and <PopoverClose> should close the content', async () => {
+    const user = userEvent.setup();
+    render(
+        <Popover>
+            <PopoverTrigger>Click to toggle</PopoverTrigger>
+            <PopoverContent>
+                Test Content
+                <PopoverClose>Close</PopoverClose>
+            </PopoverContent>
+        </Popover>
+    );
+
+    await user.click(screen.getByRole('button', { name: /Click to toggle/i }));
+    expect(screen.getByRole('button', { name: /Close/i })).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button', { name: /Close/i }));
+    expect(screen.queryByRole('button', { name: /Close/i })).not.toBeInTheDocument();
 });
