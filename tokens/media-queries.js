c8c628c153b879599bd0525fb6e0e960ff3717ff
Simon Shahriveri <si@hi-im-si.com>
2022-11-27 23:58:11 +0000

    feat(tokens): adds media query tokens

---
+++ tokens/media-queries.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'mq-mob': 360,
+    'mq-mob--med': 420,
+    'mq-mob--lrg': 560,
+
+    'mq-tab': 768,
+    'mq-tab--med': 820,
+    'mq-tab--lrg': 960,
+
+    'mq-desk': 1024,
+    'mq-desk--med': 1280,
+    'mq-desk--lrg': 1440
+};
