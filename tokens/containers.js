50d46fe027797ad88a22c7688b227d29192797ff
Steven Bister <steve.bister@openstudycollege.com>
2022-11-28 15:41:19 +0000

    feat(tokens): adds container tokens

---
+++ tokens/containers.js
@@ -0,0 +1,16 @@
+// *----------------------------------*/
+//  #CONTAINER WIDTH SETTINGS
+// *----------------------------------*/
+//
+//  Defines global container widths
+//
+// *----------------------------------*/
+module.exports = {
+    'container-size': 1440,
+    'container-size-xxxsml': 610,
+    'container-size-xxsml': 960,
+    'container-size-xsml': 1020,
+    'container-size-sml': 1140,
+    'container-size-med': 1280,
+    'container-size-lrg': 1680
+};
