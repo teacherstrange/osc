acfa3b2303b7977b8aaa47bad2da0007105175e3
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2022-12-06 15:51:36 +0000

    feat: add DropdownMenu component and stories

---
+++ packages/osc-ui/src/components/DropdownMenu/DropdownMenu.stories.tsx
@@ -0,0 +1,201 @@
+import type { Meta, Story } from '@storybook/react';
+import React, { useState } from 'react';
+
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from './DropdownMenu';
+
+import { ChevronRightIcon, HamburgerMenuIcon } from '@radix-ui/react-icons';
+
+import './dropdown-menu.scss';
+
+export default {
+    title: 'osc-ui/Dropdown Menu',
+    component: DropdownMenu,
+    parameters: {
+        docs: {
+            description: {
+                component:
+                    'Displays a menu to the user — such as a set of actions or functions — triggered by a button.',
+            },
+        },
+    },
+    argTypes: {
+        defaultOpen: {
+            name: 'defaultOpen',
+            type: 'boolean',
+            description:
+                'The open state of the dropdown menu when it is initially rendered. Use when you do not need to control its open state.',
+        },
+        open: {
+            name: 'open',
+            type: 'boolean',
+            description: `The controlled open state of the dropdown menu. Must be used in conjunction with onOpenChange.`,
+        },
+        onOpenChange: {
+            name: 'onOpenChange',
+            type: 'function',
+            description: 'Event handler called when the open state of the dropdown menu changes.',
+        },
+        modal: {
+            name: 'modal',
+            type: 'boolean',
+            description:
+                'The modality of the dropdown menu. When set to true, interaction with outside elements will be disabled and only menu content will be visible to screen readers.',
+        },
+        dir: {
+            name: 'dir',
+            type: 'string',
+            description:
+                'The reading direction of submenus when applicable. If omitted, inherits globally from DirectionProvider or assumes LTR (left-to-right) reading mode.',
+        },
+    },
+} as Meta;
+
+const dropdownMenuTrigger = (
+    <DropdownMenuTrigger asChild={true}>
+        <button className="c-dropdown-menu__icon-button" aria-label="Customise options">
+            <HamburgerMenuIcon />
+        </button>
+    </DropdownMenuTrigger>
+);
+
+const Template: Story = (args) => {
+    const { menuItems } = args;
+    return (
+        <DropdownMenu>
+            {dropdownMenuTrigger}
+            <DropdownMenuContent>
+                {menuItems.map((item, index) => (
+                    <DropdownMenuItem key={index} className="c-dropdown-menu__item">
+                        {item}
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+const TemplateWithCheckboxItems: Story = (args) => {
+    const [bookmarksChecked, setBookmarksChecked] = useState(false);
+    const [urlsChecked, setUrlsChecked] = useState(false);
+
+    return (
+        <DropdownMenu>
+            {dropdownMenuTrigger}
+            <DropdownMenuContent>
+                <DropdownMenuItem className="c-dropdown-menu__item">Item 1</DropdownMenuItem>
+                <DropdownMenuSeparator className="c-dropdown-menu__separator" />
+                <DropdownMenuCheckboxItem
+                    checked={bookmarksChecked}
+                    onCheckedChange={() => setBookmarksChecked(!bookmarksChecked)}
+                    className="c-dropdown-menu__checkbox-item"
+                >
+                    Show Bookmarks <div className="c-dropdown-menu__icon--right-slot">⌘+B</div>
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                    checked={urlsChecked}
+                    onCheckedChange={() => setUrlsChecked(!urlsChecked)}
+                    className="c-dropdown-menu__checkbox-item"
+                >
+                    Show full URLs
+                </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+const TemplateWithRadioItems: Story = (args) => {
+    const [person, setPerson] = useState('Blue');
+    const { courses } = args;
+
+    return (
+        <DropdownMenu>
+            {dropdownMenuTrigger}
+            <DropdownMenuContent>
+                <DropdownMenuItem className="c-dropdown-menu__item">
+                    New Window <div className="c-dropdown-menu__icon--right-slot">⌘+N</div>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="c-dropdown-menu__separator" />
+                <DropdownMenuRadioGroup value={person} onValueChange={setPerson}>
+                    <DropdownMenuLabel className="c-dropdown-menu__label">
+                        Colours
+                    </DropdownMenuLabel>
+                    {courses.map(({ id, value, disabled }) => (
+                        <DropdownMenuRadioItem key={id} value={value} disabled={disabled}>
+                            {value}
+                        </DropdownMenuRadioItem>
+                    ))}
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+const TemplateWithSubMenu: Story = (args) => {
+    const { submenuItems } = args;
+
+    return (
+        <DropdownMenu>
+            {dropdownMenuTrigger}
+            <DropdownMenuContent>
+                <DropdownMenuItem className="c-dropdown-menu__item">
+                    New Window <div className="c-dropdown-menu__icon--right-slot">⌘+N</div>
+                </DropdownMenuItem>
+                <DropdownMenuSub>
+                    <DropdownMenuSubTrigger className="c-dropdown-menu__sub-trigger">
+                        More Options
+                        <div className="c-dropdown-menu__icon--right-slot">
+                            <ChevronRightIcon />
+                        </div>
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                        <DropdownMenuSubContent
+                            className="c-dropdown-menu__sub-content"
+                            sideOffset={2}
+                            alignOffset={-5}
+                        >
+                            {submenuItems.map((item, index) => (
+                                <DropdownMenuItem key={index} className="c-dropdown-menu__item">
+                                    {item}
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                </DropdownMenuSub>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+export const Primary = Template.bind({});
+export const WithCheckboxItems = TemplateWithCheckboxItems.bind({});
+export const WithRadioItems = TemplateWithRadioItems.bind({});
+export const WithSubMenu = TemplateWithSubMenu.bind({});
+
+Primary.args = {
+    menuItems: ['Menu item 1', 'Menu item 2', 'Menu item 3'],
+};
+WithRadioItems.args = {
+    courses: [
+        { disabled: false, id: 1, value: 'Red' },
+        { disabled: true, id: 2, value: 'Yellow' },
+        { disabled: false, id: 2, value: 'Green' },
+        { disabled: false, id: 2, value: 'Blue' },
+    ],
+};
+WithSubMenu.args = {
+    submenuItems: ['Sub item 1', 'Sub item 2', 'Sub item 3'],
+};
---
+++ packages/osc-ui/src/components/DropdownMenu/DropdownMenu.tsx
@@ -0,0 +1,98 @@
+import type {
+    DropdownMenuCheckboxItemProps,
+    DropdownMenuContentProps,
+    DropdownMenuRadioItemProps,
+} from '@radix-ui/react-dropdown-menu';
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
+import { CheckIcon, DividerHorizontalIcon } from '@radix-ui/react-icons';
+import type { ComponentPropsWithoutRef, ElementRef, FC } from 'react';
+import React, { forwardRef } from 'react';
+
+import './dropdown-menu.scss';
+
+export const DropdownMenu = DropdownMenuPrimitive.Root;
+export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
+
+export interface ContentProps extends DropdownMenuContentProps {
+    className?: string;
+    children?: React.ReactNode;
+}
+
+export const DropdownMenuContent: FC<ContentProps> = forwardRef<
+    ElementRef<typeof DropdownMenuPrimitive.Content>,
+    ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
+>(({ children, ...props }, forwardedRef) => {
+    return (
+        <DropdownMenuPrimitive.Portal>
+            <DropdownMenuPrimitive.Content
+                className="c-dropdown-menu__content"
+                {...props}
+                ref={forwardedRef}
+            >
+                {children}
+                <DropdownMenuPrimitive.Arrow className="c-dropdown-menu__arrow" />
+            </DropdownMenuPrimitive.Content>
+        </DropdownMenuPrimitive.Portal>
+    );
+});
+
+export const DropdownMenuLabel = DropdownMenuPrimitive.Label;
+export const DropdownMenuItem = DropdownMenuPrimitive.Item;
+export const DropdownMenuGroup = DropdownMenuPrimitive.Group;
+
+export interface CheckboxProps extends DropdownMenuCheckboxItemProps {
+    className?: string;
+}
+
+export const DropdownMenuCheckboxItem: FC<CheckboxProps> = forwardRef<
+    ElementRef<typeof DropdownMenuPrimitive.CheckboxItem>,
+    ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.CheckboxItem>
+>(({ children, ...props }, forwardedRef) => {
+    return (
+        <DropdownMenuPrimitive.CheckboxItem
+            className="c-dropdown-menu__checkbox-item"
+            {...props}
+            ref={forwardedRef}
+        >
+            <DropdownMenuPrimitive.ItemIndicator className="c-dropdown-menu__indicator">
+                {props.checked === 'indeterminate' && <DividerHorizontalIcon />}
+                {props.checked === true && <CheckIcon />}
+            </DropdownMenuPrimitive.ItemIndicator>
+            {children}
+        </DropdownMenuPrimitive.CheckboxItem>
+    );
+});
+
+export const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
+
+export interface RadioProps extends DropdownMenuRadioItemProps {
+    className?: string;
+}
+
+export const DropdownMenuRadioItem: FC<RadioProps> = forwardRef<
+    ElementRef<typeof DropdownMenuPrimitive.RadioItem>,
+    ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.RadioItem>
+>(({ children, ...props }, forwardedRef) => {
+    return (
+        <DropdownMenuPrimitive.RadioItem
+            className="c-dropdown-menu__radio-item"
+            {...props}
+            ref={forwardedRef}
+        >
+            {children}
+            <DropdownMenuPrimitive.ItemIndicator>
+                <CheckIcon />
+            </DropdownMenuPrimitive.ItemIndicator>
+        </DropdownMenuPrimitive.RadioItem>
+    );
+});
+
+export const DropdownMenuPortal = DropdownMenuPrimitive.Portal;
+export const DropdownMenuSeparator = DropdownMenuPrimitive.Separator;
+export const DropdownMenuSub = DropdownMenuPrimitive.Sub;
+export const DropdownMenuSubContent = DropdownMenuPrimitive.SubContent;
+export const DropdownMenuSubTrigger = DropdownMenuPrimitive.SubTrigger;
+
+DropdownMenuContent.displayName = 'DropdownMenuContent';
+DropdownMenuCheckboxItem.displayName = 'DropdownMenuCheckboxItem';
+DropdownMenuRadioItem.displayName = 'DropdownMenuRadioItem';
