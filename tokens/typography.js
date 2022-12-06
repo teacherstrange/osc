c16e4b4c6ffa3722de6c098f2d62e90f9ce7b419
Steven Bister <steve.bister@openstudycollege.com>
2022-12-06 09:04:36 +0000

    feat(tokens): adds secondary font family

--- tokens/typography.js
+++ tokens/typography.js
@@ -8,7 +8,7 @@
 
 module.exports = {
     'font-primary': ['"Outfit", helvetica, arial, sans-serif'],
-    'font-secondary': ['"Roboto Condensed", helvetica, arial, sans-serif'],
+    'font-secondary': ['"F37 Judge", helvetica, arial, sans-serif'],
     'weight-reg': 400,
     'weight-bold': 700
 };
