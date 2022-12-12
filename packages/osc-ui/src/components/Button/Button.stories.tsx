c88178268f77d255d9f240e8a3bab0ba33426901
Steven Bister <steve.bister@openstudycollege.com>
2022-12-12 10:32:41 +0000

    feat(button): adds new stories

--- packages/osc-ui/src/components/Button/Button.stories.tsx
+++ packages/osc-ui/src/components/Button/Button.stories.tsx
@@ -12,76 +12,71 @@ export default {
         docs: {
             description: {
                 component:
-                    'Button component is used to trigger an action or event, such as submitting a form, opening a Dialog, canceling an action, or performing a delete operation.'
-            }
-        }
+                    'Button component is used to trigger an action or event, such as submitting a form, opening a Dialog, canceling an action, or performing a delete operation.',
+            },
+        },
     },
     argTypes: {
         children: {
             description: 'The content of the button.',
-            control: false
+            control: false,
         },
         as: {
             description:
                 'Set the button as an anchor element, a button element, or a Remix `<Link>` component. Leave blank for a button element.',
             control: {
-                type: 'select'
-            }
+                type: 'select',
+            },
         },
         className: {
-            description: 'Custom class'
+            description: 'Custom class',
         },
         href: {
-            description: 'Sets the url the button links to, used in conjunction with `as="a"`'
+            description: 'Sets the url the button links to, used in conjunction with `as="a"`',
         },
         isDisabled: {
             description: 'Sets the disabled state of the button and whether it is usable',
             defaultValue: false,
             table: {
                 defaultValue: {
-                    summary: false
-                }
-            }
+                    summary: false,
+                },
+            },
         },
         isLoading: {
             description: 'Sets the loading state of the button and whether it is usable',
             defaultValue: false,
             table: {
                 defaultValue: {
-                    summary: false
-                }
-            }
+                    summary: false,
+                },
+            },
         },
         loadingText: {
-            description: 'Sets the loading text of the button, used in conjunction with `isLoading`'
+            description:
+                'Sets the loading text of the button, used in conjunction with `isLoading`',
         },
         size: {
             description: 'Sets the size of the button',
             control: 'select',
-            defaultValue: 'md',
-            table: {
-                defaultValue: {
-                    summary: 'md'
-                }
-            }
         },
         target: {
             description: 'Sets the target of the button, used in conjunction with `as="a"`',
             table: {
                 type: {
-                    summary: 'string'
-                }
+                    summary: 'string',
+                },
             },
-            control: 'text'
+            control: 'text',
         },
         to: {
             description:
                 'Sets the url the button links to when using Remix `<Link>`, used in conjunction with `as="link"`',
-            control: 'text'
+            control: 'text',
         },
         type: {
             description: 'Sets the html attribute `type` of button',
-            control: 'select'
+            control: 'select',
         },
         variant: {
             description: 'Sets the style of the button, solid, outline etc.',
@@ -89,15 +84,50 @@ export default {
             defaultValue: 'solid',
             table: {
                 defaultValue: {
-                    summary: 'solid'
-                }
-            }
-        }
-    }
+                    summary: 'solid',
+                },
+            },
+        },
+    },
 } as Meta;
 
 const Template: Story<ButtonProps> = ({ children, ...args }) => (
     <Button {...args}>{children}</Button>
+);
+const VariationsTemplate: Story<ButtonProps> = ({ children, ...args }) => (
+    <ButtonGroup>
+        <Button {...args} variant="primary">
+            Primary
+        </Button>
+        <Button {...args} variant="secondary">
+            Secondary
+        </Button>
+        <Button {...args} variant="tertiary">
+            Tertiary
+        </Button>
+        <Button {...args} variant="quaternary">
+            Quaternary
+        </Button>
+        <Button {...args} variant="quinary">
+            Quinary
+        </Button>
+        <Button {...args} variant="septenary">
+            Septenary
+        </Button>
+        <Button {...args} variant="octonary">
+            Octonary
+        </Button>
+    </ButtonGroup>
+);
+const DisabledLoadingTemplate: Story<ButtonProps> = ({ children, ...args }) => (
+    <ButtonGroup>
+        <Button {...args} isDisabled>
+            {children}
+        </Button>
+        <Button {...args} isLoading>
+            {children}
+        </Button>
+    </ButtonGroup>
 );
 const IconsTemplate: Story<ButtonProps> = ({ children, ...args }) => (
     <ButtonGroup direction="column">
@@ -123,17 +153,24 @@ const CopyTemplate: Story<CopyButtonProps> = ({ children, ...args }) => (
 
 export const Primary = Template.bind({});
 Primary.args = {
-    children: 'Button',
-    variant: 'solid'
+    children: 'Special offers',
+};
+
+export const Variations = VariationsTemplate.bind({});
+Variations.args = {};
+
+export const DisabledAndLoading = DisabledLoadingTemplate.bind({});
+DisabledAndLoading.args = {
+    ...Primary.args,
 };
 
 export const IncludesIcon = IconsTemplate.bind({});
 IncludesIcon.parameters = {
     docs: {
         description: {
-            story: 'Add an icon as a child to include within the button.<br>If you wrap the text in a `<VisuallyHidden>` component you can make an accessible icon only button.'
-        }
-    }
+            story: 'Add an icon as a child to include within the button.<br>If you wrap the text in a `<VisuallyHidden>` component you can make an accessible icon only button.',
+        },
+    },
 };
 
 export const Anchor = Template.bind({});
@@ -142,14 +179,14 @@ Anchor.args = {
     children: 'External link',
     as: 'a',
     href: 'https://example.com',
-    target: '_blank'
+    target: '_blank',
 };
 Anchor.parameters = {
     docs: {
         description: {
-            story: 'Using `as="a"` transforms our button into an `<a>` tag, which means it can take any anchor element attributes such as `target="_blank"`.<br>Note: when using `target="_blank"` `rel="noopener noreferrer"` is automatically added for security purposes.'
-        }
-    }
+            story: 'Using `as="a"` transforms our button into an `<a>` tag, which means it can take any anchor element attributes such as `target="_blank"`.<br>Note: when using `target="_blank"` `rel="noopener noreferrer"` is automatically added for security purposes.',
+        },
+    },
 };
 
 export const Link = Template.bind({});
@@ -157,26 +194,26 @@ Link.args = {
     ...Primary.args,
     children: 'Internal link',
     as: 'link',
-    to: '/test-page'
+    to: '/test-page',
 };
 Link.parameters = {
     docs: {
         description: {
-            story: 'Using `as="link"` transforms our button into a `<Link>` component from Remix, which allows us to pass a url path to the `to` attribute.'
-        }
-    }
+            story: 'Using `as="link"` transforms our button into a `<Link>` component from Remix, which allows us to pass a url path to the `to` attribute.',
+        },
+    },
 };
 
 export const CopyToClipboard = CopyTemplate.bind({});
 CopyToClipboard.args = {
     ...Primary.args,
     children: 'Copy to clipboard',
-    textToCopy: 'Copied text'
+    textToCopy: 'Copied text',
 };
 CopyToClipboard.parameters = {
     docs: {
         description: {
-            story: 'The CopyButton is an extension of the `<Button>` component, which adds a copy to clipboard functionality.<br>The `textToCopy` prop is sets the text that will be copied to the clipboard when the button is clicked.'
-        }
-    }
+            story: 'The CopyButton is an extension of the `<Button>` component, which adds a copy to clipboard functionality.<br>The `textToCopy` prop is sets the text that will be copied to the clipboard when the button is clicked.',
+        },
+    },
 };
