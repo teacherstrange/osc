a46860597b79a15eedc58ebd053d6137939b9b79
Steven Bister <steve.bister@openstudycollege.com>
2022-12-15 10:54:47 +0000

    feat(switch): adds sizes to switch

--- packages/osc-ui/src/components/Switch/Switch.stories.tsx
+++ packages/osc-ui/src/components/Switch/Switch.stories.tsx
@@ -10,79 +10,108 @@ export default {
         docs: {
             description: {
                 component:
-                    'A control that allows the user to toggle between checked and not checked. Extended from the [Radix Switch primitive](https://www.radix-ui.com/docs/primitives/components/switch).'
-            }
-        }
+                    'A control that allows the user to toggle between checked and not checked. Extended from the [Radix Switch primitive](https://www.radix-ui.com/docs/primitives/components/switch).',
+            },
+        },
     },
     argTypes: {
         asChild: {
             description:
-                'Change the component to the HTML tag or custom component of the only child.'
+                'Change the component to the HTML tag or custom component of the only child.',
         },
         checked: {
             description:
                 'The controlled state of the switch. Must be used in conjunction with `onCheckedChange`.',
             type: {
                 name: 'boolean',
-                summary: 'boolean'
-            }
+                summary: 'boolean',
+            },
         },
         onCheckedChange: {
             description: 'Event handler called when the state of the switch changes.',
             type: {
                 name: 'function',
-                summary: '(checked: boolean) => void'
-            }
+                summary: '(checked: boolean) => void',
+            },
         },
         defaultChecked: {
             description:
                 'The state of the switch when it is initially rendered. Use when you do not need to control its state.',
             type: {
                 name: 'boolean',
-                summary: 'boolean'
-            }
+                summary: 'boolean',
+            },
         },
         disabled: {
             description: 'When `true`, prevents the user from interacting with the switch.',
             type: {
                 name: 'boolean',
-                summary: 'boolean'
-            }
+                summary: 'boolean',
+            },
         },
         required: {
             description:
                 'When `true`, indicates that the user must check the switch before the owning form can be submitted.',
             type: {
                 name: 'boolean',
-                summary: 'boolean'
-            }
+                summary: 'boolean',
+            },
         },
         name: {
             description:
                 'The name of the switch. Submitted with its owning form as part of a name/value pair.',
             type: {
                 name: 'string',
-                summary: 'string'
-            }
+                summary: 'string',
+            },
         },
         value: {
             description: 'The value given as data when submitted with a `name`.',
             type: {
                 name: 'string',
-                summary: 'string'
-            }
+                summary: 'string',
+            },
         },
-        className: {
-            description: 'Custom class'
+        size: {
+            control: {
+                type: 'select',
+            },
+            table: {
+                defaultValue: {
+                    summary: 'large',
+                },
+            },
         },
-        id: {
-            description: 'Custom ID'
-        }
-    }
+    },
 } as Meta;
 
 const Template: Story<Props> = (args) => <Switch {...args} />;
 
-export const Primary = Template.bind({});
+const variantsTemplate: Story<Props> = (args) => (
+    <div
+        style={{
+            display: 'flex',
+            gap: '0.5rem',
+            alignItems: 'center',
+        }}
+    >
+        <Switch {...args} size="small" />
+        <Switch {...args} size="medium" />
+        <Switch {...args} size="large" />
+    </div>
+);
 
+export const Primary = Template.bind({});
 Primary.args = {};
+
+export const Variants = variantsTemplate.bind({});
+Variants.args = {
+    ...Primary.args,
+};
+Variants.parameters = {
+    docs: {
+        description: {
+            story: 'Switches come in three sizes: `small`, `medium`, and `large`. The default size is `large`.',
+        },
+    },
+};
--- packages/osc-ui/src/components/Switch/Switch.tsx
+++ packages/osc-ui/src/components/Switch/Switch.tsx
@@ -2,20 +2,34 @@ import type { SwitchProps } from '@radix-ui/react-switch';
 import * as SwitchPrimitive from '@radix-ui/react-switch';
 import type { ComponentPropsWithoutRef, ElementRef, FC } from 'react';
 import React, { forwardRef } from 'react';
+import { useModifier } from '../../hooks/useModifier';
 import { classNames } from '../../utils/classNames';
 import './switch.scss';
 
 export interface Props extends SwitchProps {
+    /**
+     * Custom class name
+     */
     className?: string;
+    /**
+     * Custom id
+     *
+     */
     id?: string;
+    /**
+     * The size of the switch
+     * @default 'large'
+     */
+    size?: 'small' | 'medium' | 'large';
 }
 
 export const Switch: FC<Props> = forwardRef<
     ElementRef<typeof SwitchPrimitive.Root>,
     ComponentPropsWithoutRef<typeof SwitchPrimitive.Root>
 >((props: Props, forwardedRef) => {
-    const { className, id } = props;
-    const classes = classNames('c-switch', className);
+    const { className, id, size = 'large' } = props;
+    const modifier = useModifier('c-switch', size);
+    const classes = classNames('c-switch', modifier, className);
 
     return (
         <SwitchPrimitive.Root id={id} className={classes} {...props} ref={forwardedRef}>
