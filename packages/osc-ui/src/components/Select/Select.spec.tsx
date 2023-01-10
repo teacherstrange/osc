47fd16b8e407492bc649020d71fc1735d2a36139
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2023-01-10 10:19:51 +0000

    refactor: remove async from tests

--- packages/osc-ui/src/components/Select/Select.spec.tsx
+++ packages/osc-ui/src/components/Select/Select.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { Select, SelectItem } from './Select';
 
-test('should render a select component', async () => {
+test('should render a select component', () => {
     render(
         <Select name="courses-1">
             <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
@@ -11,7 +11,7 @@ test('should render a select component', async () => {
 
     expect(screen.getByRole('combobox')).toBeInTheDocument();
 });
-test('should render a disabled select component', async () => {
+test('should render a disabled select component', () => {
     const { container } = render(
         <Select disabled={true} name="courses-1">
             <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
@@ -21,7 +21,7 @@ test('should render a disabled select component', async () => {
     expect(container.querySelector(`button[data-disabled]`)).toBeInTheDocument();
     expect(container.querySelector(`button[disabled]`)).toBeInTheDocument();
 });
-test('should render a placeholder in the select component', async () => {
+test('should render a placeholder in the select component', () => {
     const { container } = render(
         <Select placeholder="Please Select" name="courses-1">
             <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
