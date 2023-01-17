eb97bc02c6c435374609a013c6e85fdfd540d73c
Steven Bister <steve.bister@openstudycollege.com>
2023-01-17 16:11:44 +0000

    chore: export components from package

--- packages/osc-ui/src/index.tsx
+++ packages/osc-ui/src/index.tsx
@@ -10,16 +10,26 @@ export { Alert, AlertDescription, AlertTitle } from './components/Alert/Alert';
 export { Avatar } from './components/Avatar/Avatar';
 export { Badge } from './components/Badge/Badge';
 export { Breadcrumb } from './components/Breadcrumb/Breadcrumb';
+export { Burger } from './components/Burger/Burger';
 export { Carousel } from './components/Carousel/Carousel';
 export { Content } from './components/Content/Content';
 export { CountdownClock } from './components/CountdownClock/CountdownClock';
 export { Footer } from './components/Footer/Footer';
-export { Header } from './components/Header/Header';
+export { Header, HeaderActionBar, HeaderNav } from './components/Header/Header';
 export { Icon, SpritesheetProvider } from './components/Icon/Icon';
 export { Image } from './components/Image/Image';
 export { ImageGallery } from './components/ImageGallery/ImageGallery';
 export { List } from './components/List/List';
 export { Modal } from './components/Modal/Modal';
+export {
+    Navbar,
+    NavContent,
+    NavItem,
+    NavLink,
+    NavList,
+    NavSubMenu,
+    NavTrigger,
+} from './components/Navbar/Navbar';
 export { Popover } from './components/Popover/Popover';
 export { SkipLink } from './components/SkipLink/SkipLink';
 export { Switch } from './components/Switch/Switch';
