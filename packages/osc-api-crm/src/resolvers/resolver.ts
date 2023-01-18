c0557f7832ca31dcf617d79e3658ac263d16d0c8
Tom Pearce <thomas.pearce@openstudycollege.com>
2023-01-18 14:48:38 +0000

    feat(crm-api): added query to get students CRM record

--- packages/osc-api-crm/src/resolvers/resolver.ts
+++ packages/osc-api-crm/src/resolvers/resolver.ts
@@ -1,18 +1,12 @@
+import { getUserById } from 'osc-api';
+import type { crmStudentArguments } from '~/types/arguments';
 import type { CrmContext } from '~/types/interfaces';
 
 export const resolvers = {
     Query: {
-        sync: async (_: undefined, __: undefined, context: CrmContext) => {
-            const { hubspot } = context;
-            const lineItem = await hubspot.crm.lineItems.basicApi.getById('2758269941', ['tutor2']);
-            console.log({ lineItem });
-
-            const result = await hubspot.crm.lineItems.basicApi.update('2758269941', {
-                properties: {
-                    tutor2: '602373870',
-                },
-            });
-            console.log({ result });
+        crmStudent: async (_: undefined, { id }: crmStudentArguments, { hubspot }: CrmContext) => {
+            const user = await getUserById(id);
+            return hubspot.crm.contacts.basicApi.getById(user!.crmLink[0].externalId.toString());
         },
     },
 };
