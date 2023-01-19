804dd567ae5a6d3ae9d5d6a39d31eb87ca215a7b
Steven Bister <steve.bister@openstudycollege.com>
2023-01-19 09:00:29 +0000

    test(burger): adds missing prop to test

--- packages/osc-ui/src/components/Burger/Burger.spec.tsx
+++ packages/osc-ui/src/components/Burger/Burger.spec.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import { Burger } from './Burger';
 
 test('renders Burger component', () => {
-    render(<Burger label="Open menu" />);
+    render(<Burger label="Open menu" isOpen={false} />);
 
     expect(screen.getByRole('button', { name: 'Open menu' })).toBeInTheDocument();
+    expect(screen.getByRole('button')).toHaveAttribute('data-state', 'closed');
 });
 
 test('renders Burger component in open state', () => {
