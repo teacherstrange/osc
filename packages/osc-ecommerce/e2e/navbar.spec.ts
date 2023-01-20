36b1d000f69a70089a098bf895c6bef3ca4ec529
Steven Bister <steve.bister@openstudycollege.com>
2023-01-20 11:21:49 +0000

    test(e2e): add e2e tests for the navbar

---
+++ packages/osc-ecommerce/e2e/navbar.spec.ts
@@ -0,0 +1,94 @@
+import { expect, test } from '@playwright/test';
+
+test('menu columns are visible', async ({ page, isMobile }) => {
+    await page.goto('/');
+
+    if (isMobile) {
+        await page.getByRole('button', { name: 'Open mobile menu' }).click();
+    }
+
+    await page.getByRole('button', { name: 'Courses' }).click();
+
+    // locate all elements with class
+    const items = page.locator('.c-nav__item--column');
+    const itemsCount = await items.count();
+
+    // loop through them one-by-one to check visibility
+    for (let i = 0; i < itemsCount; i++) {
+        const loopItem = items.nth(i);
+
+        await expect(loopItem).toBeVisible();
+    }
+});
+
+test('featured items are visible', async ({ page, isMobile }) => {
+    await page.goto('/');
+
+    if (isMobile) {
+        await page.getByRole('button', { name: 'Open mobile menu' }).click();
+    }
+
+    await page.getByRole('button', { name: 'Courses' }).click();
+
+    // locate all elements with class
+    const items = page.locator('.c-nav__item--feature');
+    const itemsCount = await items.count();
+
+    // loop through them one-by-one to check visibility
+    for (let i = 0; i < itemsCount; i++) {
+        const loopItem = items.nth(i);
+
+        await expect(loopItem).toBeVisible();
+    }
+});
+
+test('can navigate to a page in the top level of the nav', async ({ page, isMobile }) => {
+    await page.goto('/');
+
+    if (isMobile) {
+        await page.getByRole('button', { name: 'Open mobile menu' }).click();
+    }
+
+    await page.getByRole('link', { name: 'How it works' }).click();
+    await expect(page).toHaveURL('/how-it-works');
+});
+
+test('can click on the logo to go back to the homepage', async ({ page }) => {
+    await page.goto('/how-it-works');
+
+    await page.getByRole('link', { name: 'Open Study College' }).click();
+    await expect(page).toHaveURL('/');
+});
+
+test('can navigate to a page nested within the nav', async ({ page, isMobile }) => {
+    await page.goto('/');
+
+    if (isMobile) {
+        await page.getByRole('button', { name: 'Open mobile menu' }).click();
+    }
+
+    await page.getByRole('button', { name: 'Courses' }).click();
+    await page.getByRole('button', { name: 'Childcare & education' }).click();
+    await page
+        .getByRole('link', {
+            name: 'NCFE CACHE Level 3 Award in Home Based Childcare - Childminder Route',
+        })
+        .click();
+    await expect(page).toHaveURL('/products/cache-level-3-award-home-childcare-childminder');
+});
+
+test('clicking outside of the menu closes it', async ({ page, isMobile }) => {
+    if (!isMobile) {
+        await page.goto('/');
+
+        await page.getByRole('button', { name: 'Courses' }).click();
+
+        const menu = page.locator('.c-nav__content[data-state="open"]');
+
+        await expect(menu).toBeVisible();
+
+        await page.click('body');
+
+        await expect(menu).not.toBeVisible();
+    }
+});
