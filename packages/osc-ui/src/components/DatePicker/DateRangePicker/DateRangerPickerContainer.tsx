353c6372aa96efe6f52a771629324f0447dd8b97
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2023-01-25 12:54:37 +0000

    refactor: remove parseDate import

--- packages/osc-ui/src/components/DatePicker/DateRangePicker/DateRangerPickerContainer.tsx
+++ packages/osc-ui/src/components/DatePicker/DateRangePicker/DateRangerPickerContainer.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import type { FunctionComponent } from 'react';
-import { parseDate } from '@internationalized/date';
 import { DateRangePicker } from './DateRangePicker';
 import { createTimePresets } from '../utils';
 
