c32824519f653487a8ab2cc97d9bc5ade42b9e5a
Steven Bister <steve.bister@openstudycollege.com>
2023-01-27 10:23:11 +0000

    feat(ecommerce): add buttons array to content

--- packages/osc-ecommerce/app/components/Module.tsx
+++ packages/osc-ecommerce/app/components/Module.tsx
@@ -102,6 +102,7 @@ export default function Module(props: Props) {
                         paddingTop={moduleContent.paddingTop}
                         textColor={moduleContent.textColor}
                         value={moduleContent?.body}
+                        buttons={moduleContent.buttons}
                     />
                 </article>
             ) : null;
