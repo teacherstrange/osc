b19dc03e72590e1cee27a880077bbcf751e991dd
Tom Pearce <thomas.pearce@openstudycollege.com>
2023-02-08 12:04:13 +0000

    fix(api-admin): setting ID to non-nullable

--- packages/osc-api-admin/src/schemas/schema.ts
+++ packages/osc-api-admin/src/schemas/schema.ts
@@ -4,7 +4,7 @@ export const typeDefs = gql`
     extend schema @link(url: "https://specs.apollo.dev/federation/v2.0", import: ["@key", "@shareable"])
 
     type Student {
-        studentId: Int
+        studentId: Int!
         profile: StudentProfile
     }
 
