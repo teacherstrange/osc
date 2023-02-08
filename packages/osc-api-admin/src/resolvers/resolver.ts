62d147b95356fdbed7619f0ce5d991b2044ff149
Tom Pearce <thomas.pearce@openstudycollege.com>
2023-02-08 12:01:34 +0000

    fix(api-admin): removed unused context

--- packages/osc-api-admin/src/resolvers/resolver.ts
+++ packages/osc-api-admin/src/resolvers/resolver.ts
@@ -1,14 +1,9 @@
 import type { getUnassignedStudentsArgs } from '~/types/arguments';
-import type { AdminContext } from '~/types/interfaces';
 import * as admin from '~/utils/admin';
 
 export const resolvers = {
     Query: {
-        unassignedStudents: async (
-            _: undefined,
-            args: getUnassignedStudentsArgs,
-            context: AdminContext
-        ) => {
+        unassignedStudents: async (_: undefined, args: getUnassignedStudentsArgs) => {
             return await admin.unassignedStudents(args.limit);
         },
     },
