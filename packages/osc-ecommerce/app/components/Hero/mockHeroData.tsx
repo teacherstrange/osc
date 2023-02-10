14ebab65dcecd9d945451668e8bb485ff4f08a97
Steven Bister <steve.bister@openstudycollege.com>
2023-02-10 15:09:37 +0000

    test(ecommerce): adds tests for hero

---
+++ packages/osc-ecommerce/app/components/Hero/mockHeroData.tsx
@@ -0,0 +1,213 @@
+export const heroCarousel = {
+    _type: 'module.hero',
+    _key: '4274e830c3e5',
+    carouselSettings: {
+        _type: 'carouselSettings',
+        arrows: false,
+        autoplay: 'switch',
+        carouselName: 'Hero Carousel',
+        dotNav: true,
+        loop: true,
+        slidesPerView: {
+            desktop: 1,
+            mobile: 1,
+            tablet: 1,
+        },
+        startIndex: 2,
+    },
+    slides: [
+        {
+            _key: '7f21bdff41ef',
+            _type: 'heroSlide',
+            backgroundColor: 'gradient-nonary-270',
+            content: {
+                _key: null,
+                backgroundColor: null,
+                horizontalAlignment: 'left',
+                marginBottom: null,
+                paddingBottom: null,
+                paddingTop: null,
+                body: [
+                    {
+                        _key: '115e42a32649',
+                        _type: 'block',
+                        children: [
+                            {
+                                _key: '5933b05259cb0',
+                                _type: 'span',
+                                marks: ['span t-font-epsilon'],
+                                text: "With flexible payment plans and a range of special offers it's never been simpler to study.",
+                            },
+                        ],
+                        markDefs: [],
+                        style: 'normal',
+                    },
+                ],
+                buttons: [
+                    {
+                        _key: '4ddd015cf4f8',
+                        _type: 'module.button',
+                        externalLink: {
+                            newWindow: true,
+                        },
+                        label: 'Special offers',
+                        type: 'internal',
+                        variant: 'primary',
+                        documentType: 'page',
+                        slug: '/special-offers',
+                    },
+                ],
+            },
+            image: {
+                _key: '7f21bdff41ef',
+                _type: 'heroSlide',
+                alt: 'A smiling man, dancing with his left hand raised in the air',
+                src: 'https://res.cloudinary.com/de2iu8gkv/image/upload/v1675932183/c029363030ddd0a2ec5cb7d0541f4bba_yhznnx_p6av0z.avif',
+                width: 1920,
+                height: 2076,
+                responsiveImages: null,
+            },
+            title: 'Save on your study',
+            titleColor: null,
+            variant: 'primary',
+        },
+        {
+            _key: '917bca21578d',
+            _type: 'heroSlide',
+            backgroundColor: 'neutral-0',
+            content: {
+                _key: null,
+                backgroundColor: null,
+                horizontalAlignment: 'left',
+                marginBottom: null,
+                paddingBottom: null,
+                paddingTop: null,
+                body: [
+                    {
+                        _key: '245e47028091',
+                        _type: 'block',
+                        children: [
+                            {
+                                _key: '9caf437895110',
+                                _type: 'span',
+                                marks: ['span t-font-epsilon', 'span u-color-tertiary'],
+                                text: 'Get £75 off when you spend over £400 on your enrolment',
+                            },
+                        ],
+                        markDefs: [],
+                        style: 'normal',
+                    },
+                ],
+                buttons: [
+                    {
+                        _key: '907045fb13da',
+                        _type: 'module.button',
+                        externalLink: {
+                            newWindow: true,
+                        },
+                        inverse: true,
+                        isInversed: true,
+                        label: 'Special Offers',
+                        type: 'internal',
+                        variant: 'primary',
+                        documentType: 'page',
+                        slug: '/special-offers',
+                    },
+                ],
+            },
+            image: {
+                _key: '917bca21578d',
+                _type: 'heroSlide',
+                alt: null,
+                src: null,
+                width: null,
+                height: null,
+                responsiveImages: null,
+            },
+            title: 'Black Friday',
+            titleColor: 'tertiary',
+            variant: 'secondary',
+        },
+    ],
+};
+
+export const heroSingle = {
+    _type: 'module.hero',
+    _key: '4274e830c3e5',
+    carouselSettings: {
+        _type: 'carouselSettings',
+        arrows: false,
+        autoplay: 'switch',
+        carouselName: 'Hero Carousel',
+        dotNav: true,
+        loop: true,
+        slidesPerView: {
+            desktop: 1,
+            mobile: 1,
+            tablet: 1,
+        },
+        startIndex: 2,
+    },
+    slides: [
+        {
+            _key: '7f21bdff41ef',
+            _type: 'heroSlide',
+            backgroundColor: 'gradient-nonary-270',
+            content: {
+                _key: null,
+                backgroundColor: null,
+                horizontalAlignment: 'left',
+                marginBottom: null,
+                paddingBottom: null,
+                paddingTop: null,
+                body: [
+                    {
+                        _key: '115e42a32649',
+                        _type: 'block',
+                        children: [
+                            {
+                                _key: '5933b05259cb0',
+                                _type: 'span',
+                                marks: ['span t-font-epsilon'],
+                                text: "With flexible payment plans and a range of special offers it's never been simpler to study.",
+                            },
+                        ],
+                        markDefs: [],
+                        style: 'normal',
+                    },
+                ],
+                buttons: [
+                    {
+                        _key: '4ddd015cf4f8',
+                        _type: 'module.button',
+                        externalLink: {
+                            newWindow: true,
+                        },
+                        label: 'Special offers',
+                        reference: {
+                            _ref: 'f3fbb883-71a7-419f-8ff6-e0278b61e77a',
+                            _type: 'reference',
+                            _weak: true,
+                        },
+                        type: 'internal',
+                        variant: 'primary',
+                        documentType: 'page',
+                        slug: '/special-offers',
+                    },
+                ],
+            },
+            image: {
+                _key: '7f21bdff41ef',
+                _type: 'heroSlide',
+                alt: 'A smiling man, dancing with his left hand raised in the air',
+                src: 'https://res.cloudinary.com/de2iu8gkv/image/upload/v1675932183/c029363030ddd0a2ec5cb7d0541f4bba_yhznnx_p6av0z.avif',
+                width: 1920,
+                height: 2076,
+                responsiveImages: null,
+            },
+            title: 'Save on your study',
+            titleColor: null,
+            variant: 'primary',
+        },
+    ],
+};
