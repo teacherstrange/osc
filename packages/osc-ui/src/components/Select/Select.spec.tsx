c5c1ff55b8095dbbe9e62c3a616dcbdbaf578e4e
Steven Bister <steve.bister@openstudycollege.com>
2023-02-15 14:32:49 +0000

    test(select): wraps tests in the SpritesheetProvider

--- packages/osc-ui/src/components/Select/Select.spec.tsx
+++ packages/osc-ui/src/components/Select/Select.spec.tsx
@@ -1,21 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { SpritesheetProvider } from '../Icon/Icon';
 import { Select, SelectItem } from './Select';
 
 test('should render a select component', () => {
     render(
-        <Select name="courses-1">
-            <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
-        </Select>
+        <SpritesheetProvider>
+            <Select name="courses-1">
+                <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
+            </Select>
+        </SpritesheetProvider>
     );
 
     expect(screen.getByRole('combobox')).toBeInTheDocument();
 });
 test('should render a disabled select component', () => {
     const { container } = render(
-        <Select disabled={true} name="courses-1">
-            <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
-        </Select>
+        <SpritesheetProvider>
+            <Select disabled={true} name="courses-1">
+                <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
+            </Select>
+        </SpritesheetProvider>
     );
 
     expect(container.querySelector(`button[data-disabled]`)).toBeInTheDocument();
@@ -23,9 +28,11 @@ test('should render a disabled select component', () => {
 });
 test('should render a placeholder in the select component', () => {
     const { container } = render(
-        <Select placeholder="Please Select" name="courses-1">
-            <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
-        </Select>
+        <SpritesheetProvider>
+            <Select placeholder="Please Select" name="courses-1">
+                <SelectItem value="a-level-psychology"> A Level Psychology </SelectItem>
+            </Select>
+        </SpritesheetProvider>
     );
 
     expect(screen.getByText('Please Select')).toHaveTextContent('Please Select');
