8c78112f78484e6f08a6b16e73e7ac00c4f07457
Steven Bister <steve.bister@openstudycollege.com>
2023-02-21 16:11:40 +0000

    feat(footer): add mock data

---
+++ packages/osc-ui/src/components/Footer/footerContent.ts
@@ -0,0 +1,33 @@
+export const footerMenuItems = [
+    {
+        heading: 'Popular Courses',
+        items: [
+            'A Levels',
+            'GCSEs',
+            'Health & Social Care',
+            'Animal Care',
+            'Child care',
+            'Beauty Therapy',
+            'Accounting',
+        ],
+    },
+    {
+        heading: 'Learner resources',
+        items: [
+            'How it works',
+            'Tutor support',
+            'Career guides',
+            'Finance plans',
+            'FAQs',
+            'Download a prospectus',
+        ],
+    },
+    {
+        heading: 'About',
+        items: ['About us', 'Blog', 'Reviews', 'Success stories', 'Giving back'],
+    },
+    {
+        heading: 'Contact',
+        items: ['Contact us', 'Work for us', 'Corporate training', 'OSC Recruitment'],
+    },
+];
