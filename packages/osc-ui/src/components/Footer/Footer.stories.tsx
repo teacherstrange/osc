a30859d05e17a653d2c7bed21d88fc8f3d41dc62
Steven Bister <steve.bister@openstudycollege.com>
2023-02-21 11:07:33 +0000

    feat(footer): adds initial footer components

--- packages/osc-ui/src/components/Footer/Footer.stories.tsx
+++ packages/osc-ui/src/components/Footer/Footer.stories.tsx
@@ -1,14 +1,52 @@
 import type { Meta, Story } from '@storybook/react';
 import React from 'react';
-import type { Props } from './Footer';
-import { Footer } from './Footer';
+import { Icon } from '../Icon/Icon';
+import { Logo } from '../Logo/Logo';
+import type { FooterProps } from './Footer';
+import { Footer, FooterBottom, FooterGroup } from './Footer';
 
 export default {
     title: 'osc-ui/Footer',
-    component: Footer
+    component: Footer,
+    subcomponents: { FooterGroup },
 } as Meta;
 
-const Template: Story<Props> = (args) => <Footer {...args} />;
+const Template: Story<FooterProps> = (args) => (
+    <Footer {...args}>
+        <FooterGroup direction="column">
+            <Logo className="c-footer__logo" />
+
+            <div>
+                <p>
+                    Call:{' '}
+                    <a href="tel:+443308222729" className="u-text-bold">
+                        0330 822 2729
+                    </a>
+                </p>
+                <p>
+                    Email:{' '}
+                    <a href="mailto:info@openstudycollege.com" className="u-text-bold">
+                        info@openstudycollege.com
+                    </a>
+                </p>
+            </div>
+
+            <FooterGroup>
+                <Icon id="facebook" className="c-footer__icon" />
+                <Icon id="instagram" className="c-footer__icon" />
+                <Icon id="twitter" className="c-footer__icon" />
+                <Icon id="linkedin" className="c-footer__icon" />
+                <Icon id="tiktok" className="c-footer__icon" />
+                <Icon id="pinterest" className="c-footer__icon" />
+            </FooterGroup>
+        </FooterGroup>
+
+        <FooterBottom>
+            <a href="/">Terms</a>
+            <a href="/">Privacy Policy</a>
+        </FooterBottom>
+    </Footer>
+);
 
 export const Primary = Template.bind({});
 
--- packages/osc-ui/src/components/Footer/Footer.tsx
+++ packages/osc-ui/src/components/Footer/Footer.tsx
@@ -1,19 +1,75 @@
-import type { HTMLAttributes } from 'react';
+import type { HTMLAttributes, ReactNode } from 'react';
 import React from 'react';
+import { useModifier } from '../../hooks/useModifier';
 import { classNames } from '../../utils/classNames';
 import './footer.scss';
 
-export interface Props<T> extends HTMLAttributes<T> {
+export interface SharedFooterProps {
+    /**
+     * The content of the component
+     */
+    children?: ReactNode;
+    /**
+     * Custom class
+     */
     className?: string;
 }
 
-export const Footer = (props: Props<HTMLDivElement>) => {
-    const { className, ...attr } = props;
-    const classes = classNames(className);
+/* -------------------------------------------------------------------------------------------------
+ * Footer
+ * -----------------------------------------------------------------------------------------------*/
+export interface FooterProps extends SharedFooterProps, HTMLAttributes<HTMLDivElement> {}
+
+export const Footer = (props: FooterProps) => {
+    const { children, className, ...attr } = props;
+    const classes = classNames('c-footer', 'o-container', className);
 
     return (
         <footer className={classes} {...attr}>
-            Footer
+            {children}
         </footer>
+    );
+};
+
+/* -------------------------------------------------------------------------------------------------
+ * FooterGroup
+ * -----------------------------------------------------------------------------------------------*/
+export interface FooterGroupProps extends SharedFooterProps, HTMLAttributes<HTMLDivElement> {
+    /**
+     * The flow direction of the content
+     * @default row
+     */
+    direction?: 'row' | 'column';
+}
+
+export const FooterGroup = (props: FooterGroupProps) => {
+    const { children, className, direction = 'row', ...attr } = props;
+    const directionModifier = useModifier('c-footer__group', direction);
+
+    const classes = classNames('c-footer__group', directionModifier, className);
+
+    return (
+        <div className={classes} {...attr}>
+            {children}
+        </div>
+    );
+};
+
+/* -------------------------------------------------------------------------------------------------
+ * FooterBottom
+ * -----------------------------------------------------------------------------------------------*/
+export interface FooterBottomProps extends SharedFooterProps, HTMLAttributes<HTMLDivElement> {}
+
+export const FooterBottom = (props: FooterBottomProps) => {
+    const { children, className, ...attr } = props;
+
+    const classes = classNames('c-footer__btm', className);
+    const year = new Date().getFullYear();
+
+    return (
+        <div className={classes} {...attr}>
+            <div className="c-footer__btm-item">&copy; Open Study College {year}</div>
+            <div className="c-footer__btm-item">{children}</div>
+        </div>
     );
 };
