b0104b3392e563c40d32f246d0f16a890447d594
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2023-02-23 17:14:15 +0000

    refactor: update TextArea tests

--- packages/osc-ui/src/components/TextArea/TextArea.spec.tsx
+++ packages/osc-ui/src/components/TextArea/TextArea.spec.tsx
@@ -1,8 +1,15 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
 import { render } from 'test-utils';
-import { Icon } from '../Icon/Icon';
+import { SpritesheetProvider } from '../Icon/Icon';
 import { TextArea } from './TextArea';
+import { z } from 'zod';
+
+const schema = {
+    enquiry: z.object({
+        enquiry: z.string().trim().min(1),
+    }),
+};
 
 test('should render a textarea component with a label', () => {
     render(<TextArea id="enquiry" name="Enquiry" />);
@@ -17,34 +24,24 @@ test('should render an asterisk when input field is required', () => {
     expect(screen.getByRole('textbox', { name: 'Enquiry *' })).toBeInTheDocument();
 });
 
-test('should render an icon when passed as a prop', () => {
-    render(
-        <TextArea
-            icon={{
-                content: <Icon id="exclamation-mark" />,
-                label: 'Exclamation Triangle Icon',
-                type: 'error',
-            }}
-            id="enquiry"
-            name="Enquiry"
-            required={true}
-            wasSubmitted={true}
-        />
-    );
-
-    expect(document.querySelector('use')).toBeInTheDocument();
-    expect(document.querySelector('use')).toHaveAttribute(
-        'href',
-        './spritesheet.svg#exclamation-mark'
-    );
-});
-
 test('should disable the input when disabled prop is true', () => {
     render(<TextArea id="enquiry" name="Enquiry" disabled={true} />);
     expect(screen.getByRole('textbox')).toBeDisabled();
 });
 
-test('should render an error message if "wasSubmitted" is true, the field is required, and no value is present', () => {
-    render(<TextArea id="enquiry" name="Enquiry" required={true} wasSubmitted={true} />);
+test('should render an error message if an error is passed in', () => {
+    const setErrorsMock = vi.fn();
+    render(
+        <SpritesheetProvider>
+            <TextArea
+                id="enquiry"
+                name="Enquiry"
+                required={true}
+                errors={['Field is required']}
+                schema={schema.enquiry}
+                setErrors={setErrorsMock}
+            />
+        </SpritesheetProvider>
+    );
     expect(screen.getByRole('alert')).toBeInTheDocument();
 });
