95b623d02949ec6134f49af14fa063e6fe207a03
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2023-02-23 17:08:07 +0000

    refactor: update TextInput tests

--- packages/osc-ui/src/components/TextInput/TextInput.spec.tsx
+++ packages/osc-ui/src/components/TextInput/TextInput.spec.tsx
@@ -1,8 +1,16 @@
-import { ExclamationTriangleIcon, MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { SpritesheetProvider } from '../Icon/Icon';
 import { TextInput } from './TextInput';
+
+import { z } from 'zod';
+
+const schema = {
+    firstname: z.object({
+        firstname: z.string().trim().min(1, { message: 'Field is required' }),
+    }),
+};
 
 test('should render a text input component and a label', () => {
     render(<TextInput type="text" id="test-input" name="Test Input" />);
@@ -17,30 +25,27 @@ test('should render an asterisk when input field is required', () => {
 
 test('should render an icon when passed as a prop', () => {
     render(
-        <TextInput
-            type="text"
-            id="test-input"
-            name="Test Input"
-            icon={{ content: <MagnifyingGlassIcon />, label: 'Magnifying Glass' }}
-        />
+        <SpritesheetProvider>
+            <TextInput type="text" id="test-input" name="Test Input" icon={{ id: 'calendar' }} />
+        </SpritesheetProvider>
     );
-    expect(screen.getByText('Magnifying Glass')).toBeInTheDocument();
+    expect(document.querySelector('use')).toBeInTheDocument();
+    expect(document.querySelector('use')).toHaveAttribute('href', './spritesheet.svg#calendar');
 });
 
 test('should render a button with icon if action is set to type "submit" and passed an Icon', () => {
     render(
-        <TextInput
-            type="text"
-            id="test-input"
-            name="Test Input"
-            action={{
-                icon: { content: <MagnifyingGlassIcon />, label: 'Magnifying Glass' },
-                type: 'submit',
-            }}
-        />
+        <SpritesheetProvider>
+            <TextInput
+                type="text"
+                id="test-input"
+                name="Test Input"
+                action={{ iconId: 'search' }}
+            />
+        </SpritesheetProvider>
     );
-    expect(screen.getByRole('button')).toBeInTheDocument();
-    expect(screen.getByText('Magnifying Glass')).toBeInTheDocument();
+    expect(document.querySelector('use')).toBeInTheDocument();
+    expect(document.querySelector('use')).toHaveAttribute('href', './spritesheet.svg#search');
 });
 
 test('should disable the input when disabled prop is true', () => {
@@ -55,22 +60,24 @@ test('should render correct variant classes', () => {
     );
 });
 
-test('should render error message if "wasSubmitted" is true, the field is required, and no value is present', () => {
+test('should render error message if an error is passed in', () => {
+    const setErrorsMock = vi.fn();
     render(
-        <TextInput
-            type="text"
-            id="test-input"
-            icon={{
-                content: <ExclamationTriangleIcon />,
-                label: 'Exclamation Triangle Icon',
-                type: 'error',
-            }}
-            name="Test Input"
-            required={true}
-            wasSubmitted={true}
-        />
+        <SpritesheetProvider>
+            <TextInput
+                type="text"
+                id="firstname"
+                name="First Name"
+                errors={['Field is required']}
+                schema={schema.firstname}
+                required={true}
+                setErrors={setErrorsMock}
+            />
+        </SpritesheetProvider>
     );
+
     expect(screen.getByRole('alert')).toBeInTheDocument();
+    expect(screen.getByRole('alert')).toHaveTextContent('Field is required');
 });
 
 test('should change focus to second input when using the tab key', async () => {
