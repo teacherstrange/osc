c12027c45754ffa0b0d1c4ad0e783426587d6ebd
Steven Bister <steve.bister@openstudycollege.com>
2023-02-23 10:31:14 +0000

    feat(callout banner): adds footer story

--- packages/osc-ui/src/components/CalloutBanner/CalloutBanner.stories.tsx
+++ packages/osc-ui/src/components/CalloutBanner/CalloutBanner.stories.tsx
@@ -45,6 +45,30 @@ const Template: Story<CalloutBannerProps> = ({ ...args }) => (
     </div>
 );
 
+const TemplateHasButton: Story<CalloutBannerProps> = ({ ...args }) => (
+    <div className="o-container">
+        <CalloutBanner {...args}>
+            <CalloutBannerTitle className="u-color-gradient-quaternary-90">
+                OSC STUDENT DISCOUNT
+            </CalloutBannerTitle>
+            <CalloutContentGroup asChild>
+                <div className="c-content">
+                    <div className="c-content__inner c-content__inner--left">
+                        <p>
+                            We're offering a special price of £29.99 for OSC learners. To purchase
+                            the Career Kickstarter package at the discounted price please contact
+                            our learner services team via studentsupport@openstudycollege.com
+                        </p>
+                    </div>
+                </div>
+            </CalloutContentGroup>
+            <CalloutButtonGroup>
+                <Button>Buy now</Button>
+            </CalloutButtonGroup>
+        </CalloutBanner>
+    </div>
+);
+
 const HasImageTemplate: Story<CalloutBannerProps> = ({ ...args }) => (
     <div className="o-container">
         <CalloutBanner {...args}>
@@ -67,34 +91,38 @@ const HasImageTemplate: Story<CalloutBannerProps> = ({ ...args }) => (
                         </p>
                     </div>
                 </div>
-                <CalloutFooter asChild>
-                    <p className="u-color-tertiary">Offer ends 24.11.22!</p>
-                </CalloutFooter>
             </CalloutContentGroup>
         </CalloutBanner>
     </div>
 );
 
-const TemplateHasButton: Story<CalloutBannerProps> = ({ ...args }) => (
+const HasFooterTemplate: Story<CalloutBannerProps> = ({ ...args }) => (
     <div className="o-container">
         <CalloutBanner {...args}>
-            <CalloutBannerTitle className="u-color-gradient-quaternary-90">
-                OSC STUDENT DISCOUNT
-            </CalloutBannerTitle>
-            <CalloutContentGroup asChild>
+            <Image
+                src="https://res.cloudinary.com/de2iu8gkv/image/upload/v1677081736/3fa07f2f2545b5f9918b2522e9b3eee4_snzqcg.png"
+                width={330}
+                height={74}
+                alt="Black Friday"
+                className="u-self-start"
+            />
+            <CalloutContentGroup>
                 <div className="c-content">
                     <div className="c-content__inner c-content__inner--left">
-                        <p>
-                            We're offering a special price of £29.99 for OSC learners. To purchase
-                            the Career Kickstarter package at the discounted price please contact
-                            our learner services team via studentsupport@openstudycollege.com
+                        <p className="u-color-tertiary u-text-bold t-font-m">
+                            Black Friday has arrived early!
+                        </p>
+                        <p className="u-color-tertiary">
+                            <span className="u-text-bold">Save £75</span> when you spend over{' '}
+                            <span className="u-text-bold">£400</span> on your course with code{' '}
+                            <span className="u-text-bold">BF75</span>.
                         </p>
                     </div>
                 </div>
+                <CalloutFooter asChild>
+                    <p className="u-color-tertiary">Offer ends 24.11.22!</p>
+                </CalloutFooter>
             </CalloutContentGroup>
-            <CalloutButtonGroup>
-                <Button>Buy now</Button>
-            </CalloutButtonGroup>
         </CalloutBanner>
     </div>
 );
@@ -123,6 +151,19 @@ HasImage.parameters = {
     docs: {
         description: {
             story: 'Instead of text, you can pass an image as the first child by omitting the `<CalloutBannerTitle>` and placing an `<Image>` there instead.',
+        },
+    },
+};
+
+export const HasFooter = HasFooterTemplate.bind({});
+HasFooter.args = {
+    ...Primary.args,
+    className: 'u-bg-color-neutral-0',
+};
+HasFooter.parameters = {
+    docs: {
+        description: {
+            story: 'You can add a `<CalloutFooter>` subcomponent inside `<CalloutContentGroup>` to add a little extra information such as an expiry date.<br>If you need to align your title or image you can use the `u-self-start` class to pull it up to the start of the flex container.',
         },
     },
 };
