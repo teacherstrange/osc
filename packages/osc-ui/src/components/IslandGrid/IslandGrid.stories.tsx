34625b68f0dc7c2f1eb1fe1b0502822eee513ade
Steven Bister <steve.bister@openstudycollege.com>
2023-03-07 13:33:26 +0000

    refactor(island grid): update stories to use cards component

--- packages/osc-ui/src/components/IslandGrid/IslandGrid.stories.tsx
+++ packages/osc-ui/src/components/IslandGrid/IslandGrid.stories.tsx
@@ -1,5 +1,20 @@
+import { VisuallyHidden } from '@react-aria/visually-hidden';
 import type { Meta, Story } from '@storybook/react';
 import React from 'react';
+import { truncate } from '../../utils/truncate';
+import { Button } from '../Button/Button';
+import {
+    BlogCard,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    CardImage,
+    CardInner,
+    CardTitle,
+    CollectionCard,
+} from '../Card/Card';
+import { collectionCardData, collectionCardDataHzntl, postCardData } from '../Card/cardData';
+import { Image } from '../Image/Image';
 import type { IslandGridProps } from './IslandGrid';
 import { IslandGrid } from './IslandGrid';
 
@@ -19,24 +34,66 @@ export default {
 const Template: Story<IslandGridProps> = ({ ...args }) => (
     <div className="o-container">
         <IslandGrid {...args}>
-            <div
-                style={{
-                    height: '638px',
-                    backgroundImage: 'var(--color-gradient-quaternary-90)',
-                }}
-            />
-            <div
-                style={{
-                    height: '310px',
-                    backgroundImage: 'var(--color-gradient-quaternary-90)',
-                }}
-            />
-            <div
-                style={{
-                    height: '310px',
-                    backgroundImage: 'var(--color-gradient-quaternary-90)',
-                }}
-            />
+            <CollectionCard size="lg">
+                <CardImage>
+                    <Image
+                        src={collectionCardData.image.secure_url}
+                        alt={collectionCardData.image.alt}
+                        width={collectionCardData.image.width}
+                        height={collectionCardData.image.height}
+                    />
+                </CardImage>
+                <CardInner>
+                    <CardHeader>
+                        <CardTitle>{collectionCardData.title}</CardTitle>
+                    </CardHeader>
+
+                    <CardBody>
+                        <p>{truncate(collectionCardData.body)}</p>
+                        <Button isFull>23 Courses</Button>
+                    </CardBody>
+                </CardInner>
+            </CollectionCard>
+            <CollectionCard size="md">
+                <CardImage>
+                    <Image
+                        src={collectionCardDataHzntl.image.secure_url}
+                        alt={collectionCardDataHzntl.image.alt}
+                        width={collectionCardDataHzntl.image.width}
+                        height={collectionCardDataHzntl.image.height}
+                    />
+                </CardImage>
+                <CardInner>
+                    <CardHeader>
+                        <CardTitle>{collectionCardDataHzntl.title}</CardTitle>
+                    </CardHeader>
+
+                    <CardBody>
+                        <p>{truncate(collectionCardDataHzntl.body)}</p>
+                        <Button isFull>23 Courses</Button>
+                    </CardBody>
+                </CardInner>
+            </CollectionCard>
+            <CollectionCard size="md">
+                <CardImage>
+                    <Image
+                        src={collectionCardDataHzntl.image.secure_url}
+                        alt={collectionCardDataHzntl.image.alt}
+                        width={collectionCardDataHzntl.image.width}
+                        height={collectionCardDataHzntl.image.height}
+                    />
+                </CardImage>
+                <CardInner>
+                    <CardHeader>
+                        <CardTitle>{collectionCardDataHzntl.title}</CardTitle>
+                    </CardHeader>
+
+                    <CardBody>
+                        <p>{truncate(collectionCardDataHzntl.body)}</p>
+                        <Button isFull>23 Courses</Button>
+                    </CardBody>
+                </CardInner>
+            </CollectionCard>
         </IslandGrid>
     </div>
 );
@@ -44,30 +101,110 @@ const Template: Story<IslandGridProps> = ({ ...args }) => (
 const TemplateWithFour: Story<IslandGridProps> = ({ ...args }) => (
     <div className="o-container">
         <IslandGrid {...args}>
-            <div
-                style={{
-                    height: '665px',
-                    backgroundImage: 'var(--color-gradient-quaternary-90)',
-                }}
-            />
-            <div
-                style={{
-                    height: '200px',
-                    backgroundImage: 'var(--color-gradient-quaternary-90)',
-                }}
-            />
-            <div
-                style={{
-                    height: '200px',
-                    backgroundImage: 'var(--color-gradient-quaternary-90)',
-                }}
-            />
-            <div
-                style={{
-                    height: '200px',
-                    backgroundImage: 'var(--color-gradient-quaternary-90)',
-                }}
-            />
+            <BlogCard>
+                <CardImage>
+                    <Image
+                        src={postCardData.image.secure_url}
+                        alt={postCardData.image.alt}
+                        width={postCardData.image.width}
+                        height={postCardData.image.height}
+                    />
+                </CardImage>
+                <CardInner>
+                    <CardHeader>
+                        <CardTitle>{postCardData.title}</CardTitle>
+                        <CardTitle as="h3" subtitle>
+                            {postCardData.subtitle}
+                        </CardTitle>
+                    </CardHeader>
+
+                    <CardBody>
+                        <p>{truncate(postCardData.body)}</p>
+                    </CardBody>
+
+                    <CardFooter>
+                        <time dateTime="2022-10-08" className="u-text-reg">
+                            Wednesday 8th October
+                        </time>
+                        <Button variant="secondary">
+                            Read more
+                            <VisuallyHidden>about {postCardData.title}</VisuallyHidden>
+                        </Button>
+                    </CardFooter>
+                </CardInner>
+            </BlogCard>
+            <BlogCard variant="media-object">
+                <CardImage isRounded>
+                    <Image
+                        src={postCardData.image.secure_url}
+                        alt={postCardData.image.alt}
+                        width={postCardData.image.width}
+                        height={postCardData.image.height}
+                    />
+                </CardImage>
+                <CardInner>
+                    <CardHeader>
+                        <CardTitle>{postCardData.title}</CardTitle>
+                        <CardTitle as="h3" subtitle>
+                            {postCardData.subtitle}
+                        </CardTitle>
+                    </CardHeader>
+
+                    <CardFooter>
+                        <Button variant="secondary">
+                            Read more <VisuallyHidden>about {postCardData.title}</VisuallyHidden>
+                        </Button>
+                    </CardFooter>
+                </CardInner>
+            </BlogCard>
+            <BlogCard variant="media-object">
+                <CardImage isRounded>
+                    <Image
+                        src={postCardData.image.secure_url}
+                        alt={postCardData.image.alt}
+                        width={postCardData.image.width}
+                        height={postCardData.image.height}
+                    />
+                </CardImage>
+                <CardInner>
+                    <CardHeader>
+                        <CardTitle>{postCardData.title}</CardTitle>
+                        <CardTitle as="h3" subtitle>
+                            {postCardData.subtitle}
+                        </CardTitle>
+                    </CardHeader>
+
+                    <CardFooter>
+                        <Button variant="secondary">
+                            Read more <VisuallyHidden>about {postCardData.title}</VisuallyHidden>
+                        </Button>
+                    </CardFooter>
+                </CardInner>
+            </BlogCard>
+            <BlogCard variant="media-object">
+                <CardImage isRounded>
+                    <Image
+                        src={postCardData.image.secure_url}
+                        alt={postCardData.image.alt}
+                        width={postCardData.image.width}
+                        height={postCardData.image.height}
+                    />
+                </CardImage>
+                <CardInner>
+                    <CardHeader>
+                        <CardTitle>{postCardData.title}</CardTitle>
+                        <CardTitle as="h3" subtitle>
+                            {postCardData.subtitle}
+                        </CardTitle>
+                    </CardHeader>
+
+                    <CardFooter>
+                        <Button variant="secondary">
+                            Read more <VisuallyHidden>about {postCardData.title}</VisuallyHidden>
+                        </Button>
+                    </CardFooter>
+                </CardInner>
+            </BlogCard>
         </IslandGrid>
     </div>
 );
