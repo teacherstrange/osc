44cb4634218669769a72b267d793abf87611ede5
Steven Bister <steve.bister@openstudycollege.com>
2023-03-09 09:32:10 +0000

    refactor(ui): remove unused import from story

--- packages/osc-ui/src/components/Carousel/Carousel.tsx
+++ packages/osc-ui/src/components/Carousel/Carousel.tsx
@@ -1,6 +1,5 @@
 import type { KeenSliderPlugin } from 'keen-slider/react';
 import { useKeenSlider } from 'keen-slider/react';
-import { mediaQueries as mq } from 'osc-design-tokens';
 import type { ReactNode } from 'react';
 import React, { Children, useState } from 'react';
 import { classNames } from '../../utils/classNames';
