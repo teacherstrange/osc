0f1ee2f64620ab2b9557b48468af4db0a984188c
Steven Bister <steve.bister@openstudycollege.com>
2023-03-09 15:14:44 +0000

    refactor(carousel): update carousel settings in Sanity
    makes controls more consistent with other modules

--- packages/osc-ecommerce/app/queries/sanity/fragments/modules/carousel.ts
+++ packages/osc-ecommerce/app/queries/sanity/fragments/modules/carousel.ts
@@ -1,19 +1,14 @@
 import groq from 'groq';
 import { MODULE_IMAGES } from './images';
 
-// pick the info you want
-// TODO: ak add fields from props
 export const MODULE_CAROUSEL = groq`
     _key,
-    height,
-    active,
-    startIndex,
-    mediaArray[] {
-      ${MODULE_IMAGES}
+    _type,
+    carouselName,
+    slides[] {
+        "image": {
+            ${MODULE_IMAGES}
+        }
     },
-    delay,
-    slidesPerPage,
-    slideGap,
-    axis,
-    loop,
+    settings
 `;
