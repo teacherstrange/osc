4173710551ce15a3800dbf8448bebd4c2934762c
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2023-03-10 08:36:01 +0000

    refactor: rename and refactor ProductItem component

--- packages/osc-ui/src/components/Autocomplete/components/Templates.tsx
+++ packages/osc-ui/src/components/Autocomplete/components/Templates.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import '../autocomplete.scss';
+import type { AutocompleteItem as AutocompleteItemTypes } from '../types/autoComplete';
+import { Highlight } from './Highlight';
 
+// TODO - Add No results back in
 //-------------------------------------------------- //
 // No results
 // ------------------------------------------------- //
@@ -9,8 +11,9 @@ export const NoResult = () => {
     return <div>No Results</div>;
 };
 
+// TODO - Add Results Header back in
 //-------------------------------------------------- //
-// No results
+// Results Header
 // ------------------------------------------------- //
 
 interface ResultsHeaderProps {
@@ -27,92 +30,31 @@ export const ResultsHeader = (props: ResultsHeaderProps) => {
     );
 };
 
-//-------------------------------------------------- //
-// Product Item
-// ------------------------------------------------- //
+// --------------------------------------------------
+// Autocomplete SearchResultItem
+// --------------------------------------------------
 
-// TODO - Needs typing correctly
-interface ProductItemProps {
-    components?: any;
-    item?: any;
-    onParentStateChange?: any;
-    query?: any;
-    setQuery?: any;
+interface ItemProps {
+    item: AutocompleteItemTypes;
 }
 
-export const ProductItem = (props: ProductItemProps) => {
-    const { components, item, onParentStateChange, query, setQuery } = props;
+export const SearchResultItem = (props: ItemProps) => {
+    const { item } = props;
 
     return (
-        <div
-            onClick={async () => {
-                onParentStateChange(item as any);
-                setQuery(query);
-            }}
-            className="aa-ItemLink"
-        >
-            <div className="aa-ItemContent">
+        <div className="c-autocomplete__item-wrapper">
+            <div className="c-autocomplete__item-content">
                 {item.image && (
-                    <div className="aa-ItemIcon">
-                        <img
-                            src={item.image as any}
-                            alt={item.title as any}
-                            width="40"
-                            height="40"
-                        />
+                    <div className="c-autocomplete__item-image">
+                        <img src={item.image} alt={item.title} width="40" height="40" />
                     </div>
                 )}
-                <div className="aa-ItemContentBody">
-                    <div className="aa-ItemContentTitle">
-                        {item.title && <components.Highlight hit={item} attribute="title" />}
-                        {item.label && <components.Highlight hit={item} attribute="label" />}
+                <div className="c-autocomplete__item-content-body">
+                    <div className="c-autocomplete__item-title">
+                        <Highlight hit={item} attribute="title" />
                     </div>
-                    <div className="aa-ItemContentDescription">
-                        <components.Snippet hit={item} attribute="sku" />
-                    </div>
+                    <div className="c-autocomplete__item-description">{item.title}</div>
                 </div>
-            </div>
-            <div className="aa-ItemActions">
-                <button
-                    className="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
-                    type="button"
-                    title="Select"
-                >
-                    <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-                        <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    );
-};
-
-export const ProductItemCustom = (props: ProductItemProps) => {
-    const { item } = props;
-    return (
-        <div key={item.objectID} className="aa-ItemContent">
-            {item.image && (
-                <div className="aa-ItemIcon">
-                    <img src={item.image as any} alt={item.title as any} width="40" height="40" />
-                </div>
-            )}
-            <div className="aa-ItemContentBody">
-                <div className="aa-ItemContentTitle">
-                    {item.title}
-                    {item.label}
-                </div>
-                <div className="aa-ItemContentDescription"></div>
-            </div>
-            <div className="aa-ItemActions">
-                <button
-                    className="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
-                    type="button"
-                    title="Select"
-                >
-                    <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-                        <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />
-                    </svg>
-                </button>
             </div>
         </div>
     );
