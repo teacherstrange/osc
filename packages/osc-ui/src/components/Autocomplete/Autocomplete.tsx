9960d955a5b751fea828d78b512e0a42dc110030
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2023-03-10 11:24:57 +0000

    refactor: update todo list

--- packages/osc-ui/src/components/Autocomplete/Autocomplete.tsx
+++ packages/osc-ui/src/components/Autocomplete/Autocomplete.tsx
@@ -25,13 +25,15 @@ export function Autocomplete(props: Partial<AutocompleteOptions<AutocompleteItem
         status: 'idle',
     });
 
+    // TODO - Add Plugins - Popular Searches and Recently Searched
+    // TODO - Add Debouncing and Insights
+    // TODO - Create a Close button for the query
+    // TODO - Add NoResult and ResultHeader components back into the search results
+
     // TODO - Think about other things to add to autocomplete instance - e.g. Navigator,
     // TODO - Finish adding all the arg types for AutoComplete into Storybook
-    // TODO - Add NoResult and ResultHeader components back into the search results
-    // TODO - Create a Close button for the query
     // TODO - Work out how to integrate the reshape function
     // TODO - Look at whether it's useful to add 'Reacting to the Network' stuff in and the mobile experience stuff - see docs- https://www.algolia.com/doc/ui-libraries/autocomplete/guides/creating-a-renderer/
-    // TODO - Whether we want to add things like setQuery/refresh for manually updating the state - see https://www.algolia.com/doc/ui-libraries/autocomplete/core-concepts/state/ - You get these methods back from 'autocomplete'
 
     const autocomplete = useMemo(
         () =>
