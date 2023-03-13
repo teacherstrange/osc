9868bb00e6d0706fa605ee273a277504dec51a0a
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2023-03-13 17:18:23 +0000

    refactor: add form component exports

--- packages/osc-ui/src/index.tsx
+++ packages/osc-ui/src/index.tsx
@@ -30,6 +30,8 @@ export {
     CourseCard,
 } from './components/Card/Card';
 export { Carousel } from './components/Carousel/Carousel';
+export { Checkbox } from './components/Checkbox/Checkbox';
+export { ContactForm } from './components/Forms/ContactForm/ContactForm';
 export { Content } from './components/Content/Content';
 export { CountdownClock } from './components/CountdownClock/CountdownClock';
 export {
@@ -41,6 +43,7 @@ export {
     FooterMenuHeader,
     FooterMenuItem,
 } from './components/Footer/Footer';
+export { DatePicker } from './components/DatePicker/DatePicker/DatePicker';
 export { Header, HeaderActionBar, HeaderNav } from './components/Header/Header';
 export {
     Hero,
@@ -73,17 +76,21 @@ export {
     PopoverContent,
     PopoverTrigger,
 } from './components/Popover/Popover';
+export { Select, SelectItem } from './components/Select/Select';
 export { SkipLink } from './components/SkipLink/SkipLink';
 export { Switch } from './components/Switch/Switch';
 export { Tabs } from './components/Tabs/Tabs';
 export { Tag } from './components/Tag/Tag';
 export { TextGrid } from './components/TextGrid/TextGrid';
+export { TextArea } from './components/TextArea/TextArea';
+export { TextInput } from './components/TextInput/TextInput';
 export { Trustpilot } from './components/Trustpilot/Trustpilot';
 export { VideoPlayer } from './components/VideoPlayer/VideoPlayer';
 export { VisuallyHidden } from './components/VisuallyHidden/VisuallyHidden';
 // Hooks
 export { useMediaQuery } from './hooks/useMediaQuery';
 export { useSpacing } from './hooks/useSpacing';
+export { useModifier } from './hooks/useModifier';
 // Utils
 export { classNames } from './utils/classNames';
 export { formatDate as transformDate } from './utils/formatDate';
