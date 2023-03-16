bdf1351fb9fe11957163434b175e2e3b2ae6d831
Steven Bister <steve.bister@openstudycollege.com>
2023-03-16 10:28:28 +0000

    feat(hydrogen): add all cart route

---
+++ packages/osc-ecommerce/app/routes/cart.tsx
@@ -0,0 +1,39 @@
+import { Await, useMatches } from '@remix-run/react';
+import { flattenConnection } from '@shopify/hydrogen';
+import type { Cart as CartType } from '@shopify/hydrogen/storefront-api-types';
+import { Suspense } from 'react';
+
+export async function action() {
+    // TODO implement cart action
+}
+
+export default function CartRoute() {
+    const [root] = useMatches();
+    return (
+        <Suspense fallback="loading">
+            <Await resolve={root.data?.cart as CartType}>
+                {(cart) => {
+                    const linesCount = Boolean(cart?.lines?.edges?.length || 0);
+                    if (!linesCount) {
+                        return <p>Looks like you haven&rsquo;t added anything to your cart.</p>;
+                    }
+
+                    const cartLines = cart?.lines ? flattenConnection(cart?.lines) : [];
+
+                    return (
+                        <>
+                            <h1>Cart</h1>
+                            <ul>
+                                {cartLines.map((line) => (
+                                    <div key={line.id}>
+                                        <h2>{line?.merchandise?.title}</h2>
+                                    </div>
+                                ))}
+                            </ul>
+                        </>
+                    );
+                }}
+            </Await>
+        </Suspense>
+    );
+}
