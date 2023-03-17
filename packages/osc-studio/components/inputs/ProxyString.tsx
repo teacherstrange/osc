f2e3207e20cac01b17b7c153ee7332ee6cf0e56a
Steven Bister <steve.bister@openstudycollege.com>
2023-03-17 15:29:07 +0000

    refactor(studio): update sanity-shopify inputs to v3 syntax

--- packages/osc-studio/components/inputs/ProxyString.tsx
+++ packages/osc-studio/components/inputs/ProxyString.tsx
@@ -1,64 +1,34 @@
-/* eslint-disable react/display-name */
-import { FormField } from '@sanity/base/components';
 import { LockIcon } from '@sanity/icons';
 import { Box, Text, TextInput, Tooltip } from '@sanity/ui';
-import { uuid } from '@sanity/uuid';
 import get from 'lodash.get';
-import { withDocument } from 'part:@sanity/form-builder';
-import React, { forwardRef } from 'react';
+import React from 'react';
+import type { SanityDocument, StringInputProps, StringSchemaType } from 'sanity';
+import { useFormValue } from 'sanity';
 
-// TODO: type correctly
-type Props = any;
+type Props = StringInputProps<StringSchemaType & { options?: { field?: string } }>;
 
-const ProxyString = forwardRef<HTMLInputElement, Props>((props, ref) => {
-    const {
-        compareValue, // Value to check for "edited" functionality
-        document,
-        markers,
-        onFocus,
-        onBlur,
-        placeholder,
-        presence,
-        type
-    } = props;
+const ProxyString = (props: Props) => {
+    const { schemaType } = props;
 
-    const path = type?.options?.field;
-    const proxyValue = get(document, path);
+    const path = schemaType?.options?.field;
+    const doc = useFormValue([]) as SanityDocument;
 
-    const inputId = uuid();
+    const proxyValue = path ? (get(doc, path) as string) : '';
 
     return (
-        <FormField
-            compareValue={compareValue}
-            description={type?.description}
-            inputId={inputId}
-            markers={markers}
-            presence={presence}
-            title={type?.title}
+        <Tooltip
+            content={
+                <Box padding={2}>
+                    <Text muted size={1}>
+                        This value is set in Shopify (<code>{path}</code>)
+                    </Text>
+                </Box>
+            }
+            portal
         >
-            <Tooltip
-                content={
-                    <Box padding={2}>
-                        <Text muted size={1}>
-                            This value is set in Shopify (<code>{path}</code>)
-                        </Text>
-                    </Box>
-                }
-                portal
-            >
-                <TextInput
-                    iconRight={LockIcon}
-                    id={inputId}
-                    onBlur={onBlur}
-                    onFocus={onFocus}
-                    placeholder={placeholder}
-                    readOnly={true}
-                    ref={ref}
-                    value={proxyValue}
-                />
-            </Tooltip>
-        </FormField>
+            <TextInput iconRight={LockIcon} readOnly={true} value={proxyValue} />
+        </Tooltip>
     );
-});
+};
 
-export default withDocument(ProxyString);
+export default ProxyString;
