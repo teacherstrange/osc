b58bb998102c70ce78a9a620d5f1bce5a0aea2a1
Steven Bister <steve.bister@openstudycollege.com>
2023-03-17 15:23:59 +0000

    feat(studio): add shopify document types to constants

--- packages/osc-studio/constants.js
+++ packages/osc-studio/constants.js
@@ -12,6 +12,12 @@ export const LOCKED_DOCUMENT_IDS = ['home', 'settings', 'blog'];
 // - cannot be duplicated, unpublished or deleted
 export const LOCKED_DOCUMENT_TYPES = ['media.tag'];
 
+// Document types which:
+// - cannot be created in the 'new document' menu
+// - cannot be duplicated, unpublished or deleted
+// - are from the Sanity Connect Shopify app - and can be linked to on Shopify
+export const SHOPIFY_DOCUMENT_TYPES = ['product', 'productVariant', 'collection'];
+
 // References to include in 'internal' links
 export const PAGE_REFERENCES = [
     { type: 'collection' },
