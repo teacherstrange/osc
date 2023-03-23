c152535a8a70ddd2c961c3880c48029a8cc6253d
Steven Bister <steve.bister@openstudycollege.com>
2023-03-23 10:02:32 +0000

    refactor(studio): add defineField to seo modules for better ts support

--- packages/osc-studio/schemas/objects/seo/page.tsx
+++ packages/osc-studio/schemas/objects/seo/page.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import { defineField, defineType } from 'sanity';
 
-export default {
+export default defineType({
     name: 'seo.page',
     title: 'SEO',
     type: 'object',
     options: {
         collapsed: false,
-        collapsible: true
+        collapsible: true,
     },
     fields: [
-        {
+        defineField({
             name: 'title',
             title: 'Title',
             type: 'placeholderString',
@@ -20,59 +20,59 @@ export default {
             ),
             options: { field: 'title' },
             validation: (Rule) =>
-                Rule.max(50).warning('Longer titles may be truncated by search engines')
-        },
-        {
+                Rule.max(50).warning('Longer titles may be truncated by search engines'),
+        }),
+        defineField({
             name: 'canonicalUrl',
             title: 'Canonical Url',
             type: 'url',
             description: 'Leave blank to use the default url for this page',
             validation: (Rule) =>
                 Rule.uri({
-                    scheme: ['http', 'https']
-                })
-        },
-        {
+                    scheme: ['http', 'https'],
+                }),
+        }),
+        defineField({
             name: 'keywords',
             title: 'Keywords',
             type: 'string',
-            description: 'Separate with commas'
-        },
-        {
+            description: 'Separate with commas',
+        }),
+        defineField({
             name: 'synonyms',
             title: 'Synonyms',
             type: 'string',
-            description: 'Similar words to inform the SEO review'
-        },
-        {
+            description: 'Similar words to inform the SEO review',
+        }),
+        defineField({
             name: 'description',
             title: 'Description',
             type: 'text',
             rows: 2,
             validation: (Rule) =>
-                Rule.max(150).warning('Longer descriptions may be truncated by search engines')
-        },
-        {
+                Rule.max(150).warning('Longer descriptions may be truncated by search engines'),
+        }),
+        defineField({
             name: 'image',
             title: 'Image',
-            type: 'image'
-        },
-        {
+            type: 'image',
+        }),
+        defineField({
             name: 'robots',
             title: 'Search engine visibility',
             type: 'object',
             description: 'It is up to search engines to honor this request.',
             options: {
                 collapsed: false,
-                collapsible: true
+                collapsible: true,
             },
             fields: [
-                {
+                defineField({
                     name: 'noIndex',
                     title: 'Discourage search engines from indexing this page',
-                    type: 'boolean'
-                }
-            ]
-        }
-    ]
-};
+                    type: 'boolean',
+                }),
+            ],
+        }),
+    ],
+});
