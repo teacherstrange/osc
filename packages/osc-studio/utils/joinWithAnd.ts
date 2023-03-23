dc299fba06e7b42c9c7af3845c4e27ce69adeef6
Steven Bister <steve.bister@openstudycollege.com>
2023-03-23 12:13:34 +0000

    fix(studio): adds ignore to expected type

--- packages/osc-studio/utils/joinWithAnd.ts
+++ packages/osc-studio/utils/joinWithAnd.ts
@@ -1,4 +1,5 @@
 export const joinWithAnd = (list: string[]) => {
+    // @ts-ignore -- note sure why ListFormat isn't recognized?
     const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
 
     return formatter.format(list);
