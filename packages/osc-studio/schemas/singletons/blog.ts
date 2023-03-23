8a91cbd1982b08261c066e311d239a2a2115d343
Steven Bister <steve.bister@openstudycollege.com>
2023-03-23 10:55:42 +0000

    fix(studio): remove initial value

--- packages/osc-studio/schemas/singletons/blog.ts
+++ packages/osc-studio/schemas/singletons/blog.ts
@@ -36,7 +36,6 @@ export default defineType({
             type: 'slug',
             options: { source: 'title' },
             validation: validateSlug,
-            initialValue: TITLE.toLowerCase(),
         }),
         // Show hero
         defineField({
