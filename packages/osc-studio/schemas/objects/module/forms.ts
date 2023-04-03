5a3d945a8f82c21a23dd2ff55cf2367df5ff4946
Matthew Wendzina <matthew.wendzina@openstudycollege.com>
2023-04-03 08:37:44 +0100

    refactor: fix module.forms and add Callback Form Id

--- packages/osc-studio/schemas/objects/module/forms.ts
+++ packages/osc-studio/schemas/objects/module/forms.ts
@@ -1,13 +1,15 @@
 import { DocumentIcon } from '@sanity/icons';
+import { defineField, defineType } from 'sanity';
 
 // TODO - Update so that these are pulled in dynamically on a get request to Hubspot form API. Looks like a proxy needs setting up in order to do this.
 const forms = [
     { name: 'Newsletter Form', id: '9cf24fde-b8c4-4165-bff6-1a151b83259d' },
     { name: 'Contact Form', id: 'b6aca185-96b6-416f-9b03-9ce8f894ea44' },
     { name: 'Prospectus Form', id: 'ec135217-340a-46cf-93b7-59a2ab1e509e' },
+    { name: 'Callback Form', id: 'fe5ae92f-faeb-4d04-b8e0-72619d5459f5' },
 ];
 
-export default {
+export default defineType({
     name: 'module.forms',
     title: 'Forms',
     type: 'object',
@@ -23,29 +25,28 @@ export default {
         },
     ],
     fields: [
-        {
+        defineField({
             name: 'formNameAndId',
             title: 'Form',
             description: 'List of forms pulled in from HubSpot',
             type: 'string',
             options: {
-                layout: 'select',
+                layout: 'dropdown',
                 list: forms.map((form) => ({
                     title: form.name,
                     value: `${form.name}, ${form.id}`,
                 })),
             },
             group: 'forms',
-        },
-        {
-            default: false,
+        }),
+        defineField({
             name: 'slideOut',
             title: 'Slide Out',
             description: 'Whether the form should slide out',
             type: 'boolean',
             group: 'settings',
-        },
-        {
+        }),
+        defineField({
             name: 'slideDirection',
             title: 'Slide Direction',
             description: 'Whether the form should slide out to the right or the left',
@@ -55,14 +56,14 @@ export default {
                 layout: 'dropdown',
             },
             group: 'settings',
-        },
-        {
+        }),
+        defineField({
             name: 'slideOutText',
             title: 'Slide Out Text',
             description: 'Optional text for slide out if selected',
             type: 'string',
             group: 'settings',
-        },
+        }),
     ],
     preview: {
         select: {
@@ -76,4 +77,4 @@ export default {
             };
         },
     },
-};
+});
