f7e4099a2c49a669d13a5ba793b18a84b9952392
Steven Bister <steve.bister@openstudycollege.com>
2023-04-03 16:51:01 +0100

    feat(ecommerce): setup preview config

--- packages/osc-ecommerce/app/lib/sanity/config.ts
+++ packages/osc-ecommerce/app/lib/sanity/config.ts
@@ -1,5 +1,5 @@
 // Becuase this needs to work on both the server and the browser we need to check for the document being defined https://remix.run/docs/en/v1/guides/constraints#document-guard
-
+import type { PreviewConfig } from '@sanity/preview-kit';
 import invariant from 'tiny-invariant';
 
 // The document.env should be set in the root.tsx file so we can access it across all routes
@@ -20,5 +20,20 @@ export const config = {
     apiVersion: '2021-10-21', // see studio/contstants.js
     projectId: SANITY_STUDIO_API_PROJECT_ID,
     dataset: SANITY_STUDIO_API_DATASET,
-    useCdn: true
+    useCdn: true,
+};
+
+export const previewConfig: PreviewConfig = {
+    ...config,
+    // The limit on number of documents, to prevent using too much memory unexpectedly
+    // It's 3000 by default, increase or decrease as needed and use `includeTypes` to further optimize the performance
+    documentLimit: 3000,
+    // Optional allow list filter for document types. You can use this to limit the amount of documents by declaring the types you want to sync. Note that since you're fetching a subset of your dataset, queries that works against your Content Lake might not work against the local groq-store.
+    includeTypes: ['home', 'blog', 'collection', 'page', 'post', 'product'],
+    // By default documents that are "draft" are overlayed with their published counterparts.
+    // This lets you simulate what your app will look like after the drafts are published.
+    // If your queries are already equipped to handle drafts vs published
+    // or you otherwise show UI depending on draft status set this to false
+    overlayDrafts: true,
+    onPublicAccessOnly: () => alert('You are not logged in. You will only see public data.'),
 };
