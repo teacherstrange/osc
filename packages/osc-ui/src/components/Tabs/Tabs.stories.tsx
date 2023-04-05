607b32cd25db1a044eb38d1128cc71a702cc84dd
Steven Bister <steve.bister@openstudycollege.com>
2023-04-05 13:00:13 +0100

    refactor(tabs): update tabs to be composable

--- packages/osc-ui/src/components/Tabs/Tabs.stories.tsx
+++ packages/osc-ui/src/components/Tabs/Tabs.stories.tsx
@@ -1,42 +1,62 @@
 import type { Meta, Story } from '@storybook/react';
 import React from 'react';
-import type { Props } from './Tabs';
-import { Tabs } from './Tabs';
+import type { TabProps } from './Tabs';
+import { TabContent, TabList, TabTrigger, Tabs } from './Tabs';
 
 export default {
     title: 'osc-ui/Tabs',
     component: Tabs,
+    subcomponents: {
+        TabContent,
+        TabList,
+        TabTrigger,
+    },
     parameters: {
         docs: {
             description: {
                 component:
-                    'A set of layered sections of content — known as tab panels — that are displayed one at a time.'
-            }
-        }
-    }
+                    'A set of layered sections of content — known as tab panels — that are displayed one at a time.',
+            },
+        },
+    },
 } as Meta;
 
-const Template: Story<Props> = (args) => <Tabs {...args} />;
-
-export const Primary = Template.bind({});
-
-Primary.args = {
-    tabs: [
+const Template: Story<TabProps> = (args) => {
+    const tabs = [
         {
             key: '1',
             list: 'One',
-            panel: 'one!'
+            panel: 'one!',
         },
         {
             key: '2',
             list: 'Two',
-            panel: 'two!'
+            panel: 'two!',
         },
         {
             key: '3',
             list: 'Three',
-            panel: 'three!'
-        }
-    ],
-    className: ''
+            panel: 'three!',
+        },
+    ];
+
+    return (
+        <Tabs defaultValue={tabs[0].key} {...args}>
+            <TabList>
+                {tabs.map((tab) => (
+                    <TabTrigger value={tab.key} key={tab.key}>
+                        {tab.list}
+                    </TabTrigger>
+                ))}
+            </TabList>
+            {tabs.map((tab) => (
+                <TabContent value={tab.key} key={tab.key}>
+                    {tab.panel}
+                </TabContent>
+            ))}
+        </Tabs>
+    );
 };
+
+export const Primary = Template.bind({});
+Primary.args = {};
--- packages/osc-ui/src/components/Tabs/Tabs.tsx
+++ packages/osc-ui/src/components/Tabs/Tabs.tsx
@@ -1,36 +1,98 @@
-import * as TabPrimitives from '@radix-ui/react-tabs';
-import React from 'react';
+import * as TabPrimitive from '@radix-ui/react-tabs';
+import type { ComponentPropsWithoutRef, ElementRef } from 'react';
+import React, { forwardRef } from 'react';
 import { classNames } from '../../utils/classNames';
+
 import './tabs.scss';
 
-export interface Props {
-    tabs: {
-        key: string;
-        list: string;
-        panel: string;
-    }[];
+export interface SharedTabProps {
+    /**
+     * Custom class
+     */
     className?: string;
 }
 
-export const Tabs = (props: Props) => {
-    const { tabs, className, ...other } = props;
-    const classes = classNames('c-tabs', className);
+/* -------------------------------------------------------------------------------------------------
+ * Tabs
+ * -----------------------------------------------------------------------------------------------*/
+export interface TabProps
+    extends ComponentPropsWithoutRef<typeof TabPrimitive.Root>,
+        SharedTabProps {}
 
-    return (
-        <TabPrimitives.Root className={classes} {...other}>
-            <TabPrimitives.List>
-                {tabs.map((tab, index) => (
-                    <TabPrimitives.Trigger key={tab.key} value={`${index}`}>
-                        {tab.list}
-                    </TabPrimitives.Trigger>
-                ))}
-            </TabPrimitives.List>
+export const Tabs = forwardRef<ElementRef<typeof TabPrimitive.Root>, TabProps>(
+    (props, forwardedRef) => {
+        const { children, className, ...rest } = props;
+        const classes = classNames('c-tabs', className);
 
-            {tabs.map((tab, index) => (
-                <TabPrimitives.Content key={tab.key} value={`${index}`}>
-                    {tab.panel}
-                </TabPrimitives.Content>
-            ))}
-        </TabPrimitives.Root>
-    );
-};
+        return (
+            <TabPrimitive.Root className={classes} {...rest} ref={forwardedRef}>
+                {children}
+            </TabPrimitive.Root>
+        );
+    }
+);
+Tabs.displayName = 'Tabs';
+
+/* -------------------------------------------------------------------------------------------------
+ * Tab List
+ * -----------------------------------------------------------------------------------------------*/
+export interface TabListProps
+    extends ComponentPropsWithoutRef<typeof TabPrimitive.List>,
+        SharedTabProps {}
+
+export const TabList = forwardRef<ElementRef<typeof TabPrimitive.List>, TabListProps>(
+    (props, forwardedRef) => {
+        const { children, className, ...rest } = props;
+        const classes = classNames('c-tabs__list', className);
+
+        return (
+            <TabPrimitive.List className={classes} {...rest} ref={forwardedRef}>
+                {children}
+            </TabPrimitive.List>
+        );
+    }
+);
+TabList.displayName = 'TabList';
+
+/* -------------------------------------------------------------------------------------------------
+ * Tab Trigger
+ * -----------------------------------------------------------------------------------------------*/
+export interface TabTriggerProps
+    extends ComponentPropsWithoutRef<typeof TabPrimitive.Trigger>,
+        SharedTabProps {}
+
+export const TabTrigger = forwardRef<ElementRef<typeof TabPrimitive.Trigger>, TabTriggerProps>(
+    (props, forwardedRef) => {
+        const { children, className, ...rest } = props;
+        const classes = classNames('c-tabs__trigger u-text-uppercase u-text-bold', className);
+
+        return (
+            <TabPrimitive.Trigger className={classes} {...rest} ref={forwardedRef}>
+                {children}
+            </TabPrimitive.Trigger>
+        );
+    }
+);
+TabTrigger.displayName = 'TabTrigger';
+
+/* -------------------------------------------------------------------------------------------------
+ * Tab Content
+ * -----------------------------------------------------------------------------------------------*/
+export interface TabContentProps
+    extends ComponentPropsWithoutRef<typeof TabPrimitive.Content>,
+        SharedTabProps {}
+
+export const TabContent = forwardRef<ElementRef<typeof TabPrimitive.Content>, TabContentProps>(
+    (props, forwardedRef) => {
+        const { children, className, ...rest } = props;
+        const classes = classNames('c-tabs__content', className);
+
+        return (
+            // Force mount so content is findable on the page
+            <TabPrimitive.Content className={classes} forceMount {...rest} ref={forwardedRef}>
+                {children}
+            </TabPrimitive.Content>
+        );
+    }
+);
+TabContent.displayName = 'TabContent';
