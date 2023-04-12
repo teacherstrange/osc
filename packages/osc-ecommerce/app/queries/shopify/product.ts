4da5f82690dd5b3641704ed99ba8268b091985ae
Steven Bister <steve.bister@openstudycollege.com>
2023-04-12 16:11:11 +0100

    feat(product page): update product query to include full variant fragment

--- packages/osc-ecommerce/app/queries/shopify/product.ts
+++ packages/osc-ecommerce/app/queries/shopify/product.ts
@@ -1,17 +1,62 @@
+const PRODUCT_VARIANT_FRAGMENT = `#graphql
+  fragment ProductVariantFragment on ProductVariant {
+    id
+    availableForSale
+    selectedOptions {
+      name
+      value
+    }
+    image {
+      id
+      url
+      altText
+      width
+      height
+    }
+    price {
+      amount
+      currencyCode
+    }
+    compareAtPrice {
+      amount
+      currencyCode
+    }
+    sku
+    title
+    unitPrice {
+      amount
+      currencyCode
+    }
+    product {
+      title
+      handle
+    }
+  }
+`;
+
 export const PRODUCT_QUERY = `#graphql
+  ${PRODUCT_VARIANT_FRAGMENT}
   query Product(
     $country: CountryCode
     $language: LanguageCode
     $handle: String!
+    # $selectedOptions: [SelectedOptionInput!]!
   ) @inContext(country: $country, language: $language) {
     product(handle: $handle) {
         id
         title
         descriptionHtml
         vendor
-        variants(first: 3) {
+        options {
+            name
+            values
+        }
+        # selectedVariant: variantBySelectedOptions(selectedOptions: $selectedOptions) {
+        #     ...ProductVariantFragment
+        # }
+        variants(first: 10) {
             nodes {
-                sku
+                ...ProductVariantFragment
             }
         }
     }
