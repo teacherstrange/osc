35558b8f3f8cce3bda429c227f81ff390af61737
Steven Bister <steve.bister@openstudycollege.com>
2023-04-18 16:30:34 +0100

    feat(price): wrap price in selectedVariant check so it will quietly fail

--- packages/osc-ecommerce/app/components/Price/Price.tsx
+++ packages/osc-ecommerce/app/components/Price/Price.tsx
@@ -18,21 +18,24 @@ export const Price = (props: PriceProps) => {
     return (
         <div className={`c-price__container ${isOnSale ? 'has-sale-price' : ''}`}>
             <div className="o-flex">
-                <span className="c-price t-font-xl u-text-bold u-color-primary u-mb-0">
-                    {isOnSale ? (
-                        <Money
-                            withoutTrailingZeros
-                            data={selectedVariant?.compareAtPrice!}
-                            as="span"
-                            className="c-price c-price--strike u-text-reg u-color-neutral-600 u-mb-0"
-                        />
-                    ) : null}
-                    {/* // TODO: This "in full" text will need to be dynamic once we have the option to offer monthly payments */}
-                    <Money withoutTrailingZeros data={selectedVariant?.price!} as="span" /> in full
-                </span>
+                {selectedVariant ? (
+                    <span className="c-price t-font-xl u-text-bold u-color-primary u-mb-0">
+                        {isOnSale ? (
+                            <Money
+                                withoutTrailingZeros
+                                data={selectedVariant?.compareAtPrice!}
+                                as="span"
+                                className="c-price c-price--strike u-text-reg u-color-neutral-600 u-mb-0"
+                            />
+                        ) : null}
+                        {/* // TODO: This "in full" text will need to be dynamic once we have the option to offer monthly payments */}
+                        <Money withoutTrailingZeros data={selectedVariant?.price!} as="span" /> in
+                        full
+                    </span>
+                ) : null}
             </div>
 
-            <span>Course code: {selectedVariant.sku}</span>
+            <span>Course code: {selectedVariant?.sku}</span>
         </div>
     );
 };
