33ff5481f467a45139ab9c1faa1e5fea13b281f4
Steven Bister <steve.bister@openstudycollege.com>
2023-04-21 10:28:53 +0100

    refactor(ecommerce): update page preview component to allow more flexibility in returned component

--- packages/osc-ecommerce/app/components/PagePreview.tsx
+++ packages/osc-ecommerce/app/components/PagePreview.tsx
@@ -2,15 +2,22 @@ import type { UsePreview } from '@sanity/preview-kit';
 import { definePreview } from '@sanity/preview-kit';
 import { previewConfig } from '~/lib/sanity/config';
 import type { PreviewProps } from '~/types/sanity';
-import { PageContent } from './PageContent';
+import type { PageContent, PageContentUpper } from './PageContent';
+
+interface PagePreviewProps extends Omit<PreviewProps, 'token'> {
+    /**
+     * Component that holds the page data
+     */
+    Component: typeof PageContent | typeof PageContentUpper;
+}
 
 // Export as a default function so we can use with react.lazy
-export default function PagePreview(props: Omit<PreviewProps, 'token'>) {
-    const { query, params } = props;
+export default function PagePreview(props: PagePreviewProps) {
+    const { query, params, Component } = props;
 
     const data = usePreview(null, query, params);
 
-    return <PageContent {...data[0]} />;
+    return <Component {...data[0]} />;
 }
 
 const usePreview: UsePreview = definePreview(previewConfig);
