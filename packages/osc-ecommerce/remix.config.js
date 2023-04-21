1d4c2f54a534ac4035c37132b52934ab14661d7a
Steven Bister <steve.bister@openstudycollege.com>
2023-04-21 15:04:47 +0100

    chore(ecommerce): enable v2 future flag for replacing useTransition

--- packages/osc-ecommerce/remix.config.js
+++ packages/osc-ecommerce/remix.config.js
@@ -10,4 +10,7 @@ module.exports = {
     serverDependenciesToBundle: [
         /^osc-ui\/dist\/.*\.svg$/, // match svg files in dist folder, this prevents the unhandled token error being thrown
     ],
+    future: {
+        v2_normalizeFormMethod: true,
+    },
 };
