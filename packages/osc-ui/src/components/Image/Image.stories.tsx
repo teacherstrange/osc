4ae397b26889025cd66f836eca45771841a15efd
Steven Bister <steve.bister@openstudycollege.com>
2023-04-27 15:13:50 +0100

    feat(img): update storybook

--- packages/osc-ui/src/components/Image/Image.stories.tsx
+++ packages/osc-ui/src/components/Image/Image.stories.tsx
@@ -11,56 +11,90 @@ export default {
         docs: {
             description: {
                 component:
-                    'Image component for transforming and displaying responsive images from Cloudinary.'
-            }
-        }
+                    'Image component for transforming and displaying responsive images from Cloudinary.',
+            },
+        },
     },
     argTypes: {
         _key: {
             // Only included to prevent typescipt type overlay error
             // Not needed for storybook
             table: {
-                disable: true
-            }
+                disable: true,
+            },
         },
         _type: {
             // Only included to prevent typescipt type overlay error
             // Not needed for storybook
             table: {
-                disable: true
-            }
+                disable: true,
+            },
         },
         alt: {
-            description: 'Sets the alternative text'
+            description: 'Sets the alternative text',
         },
         artDirectedImages: {
             description:
-                'An array of image objects to generate the `<picture>` version of the image.'
+                'An array of image objects to generate the `<picture>` version of the image.',
         },
         className: {
-            description: 'Custom class'
+            description: 'Custom class',
         },
         height: {
             description:
-                'Sets the height attribute. Important for hinting the aspect ratio to the browser therefore reducing/preventing CLS'
+                'Sets the height attribute. Important for hinting the aspect ratio to the browser therefore reducing/preventing CLS',
         },
         loading: {
-            description: 'Sets the native loading attribute'
+            description: 'Sets the native loading attribute',
         },
         responsiveWidths: {
-            description: 'Generates the different image sizes within the srcset attribute'
+            description: 'Generates the different image sizes within the srcset attribute',
         },
         sizes: {
-            description: 'Sets the sizes attribute on the image'
+            description: 'Sets the sizes attribute on the image',
         },
         src: {
-            description: 'Sets the image url, this must be a cloudinary image url'
+            description: 'Sets the image url, this must be a cloudinary image url',
         },
         width: {
             description:
-                'Sets the width attribute. Important for hinting the aspect ratio to the browser therefore reducing/preventing CLS'
-        }
-    }
+                'Sets the width attribute. Important for hinting the aspect ratio to the browser therefore reducing/preventing CLS',
+        },
+        isGrayScale: {
+            description: 'Toggles the grayscale effect on the image',
+        },
+        overlayColor: {
+            description: 'Sets a colour to overlay the image',
+            options: [
+                'primary',
+                'secondary',
+                'tertiary',
+                'quaternary',
+                'quinary',
+                'senary',
+                'septenary',
+                'octonary',
+                'nonary',
+                'denary',
+                'duodenary',
+                'gradient-primary',
+                'gradient-secondary',
+                'gradient-tertiary',
+                'gradient-quaternary',
+                'gradient-quinary',
+                'gradient-senary',
+                'gradient-septenary',
+                'gradient-octonary',
+                'gradient-nonary',
+            ],
+            control: {
+                type: 'select',
+            },
+        },
+        hasTransparency: {
+            description: 'Toggles a transparency effect on the image',
+        },
+    },
 } as Meta;
 
 const Template: Story<Props<HTMLImageElement>> = (args) => <Image {...args} />;
@@ -71,20 +105,20 @@ Primary.args = {
     alt: imageData.alt,
     src: imageData.src,
     height: imageData.height,
-    width: imageData.width
+    width: imageData.width,
 };
 
 export const HasArtDirectedImages = Template.bind({});
 HasArtDirectedImages.args = {
     ...Primary.args,
-    artDirectedImages: imageData.responsiveImages
+    artDirectedImages: imageData.responsiveImages,
 };
 HasArtDirectedImages.parameters = {
     docs: {
         description: {
-            story: 'Resize the browser to see the different images.'
-        }
-    }
+            story: 'Resize the browser to see the different images.',
+        },
+    },
 };
 
 export const HasNoTransformations = Template.bind({});
@@ -92,12 +126,27 @@ HasNoTransformations.args = {
     altText: imageDataNoTransforms.alt,
     src: imageDataNoTransforms.src,
     height: imageDataNoTransforms.height,
-    width: imageDataNoTransforms.width
+    width: imageDataNoTransforms.width,
 };
 HasNoTransformations.parameters = {
     docs: {
         description: {
-            story: 'If no image transformations have been made, then resized images and srcset will still be applied'
-        }
-    }
+            story: 'If no image transformations have been made, then resized images and srcset will still be applied',
+        },
+    },
+};
+
+export const CSSTransformations = Template.bind({});
+CSSTransformations.args = {
+    ...Primary.args,
+    isGrayScale: true,
+    overlayColor: 'gradient-quaternary',
+    hasTransparency: true,
+};
+CSSTransformations.parameters = {
+    docs: {
+        description: {
+            story: 'Apply CSS transformations to the image by using the `isGrayscale`, `hasTransparency` and/or the `overlayColor` props.',
+        },
+    },
 };
