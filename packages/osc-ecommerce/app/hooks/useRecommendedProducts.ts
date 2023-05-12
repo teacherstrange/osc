dd95eb0a4ea6dedaba0594c341a2032e11b863d1
Steven Bister <steve.bister@openstudycollege.com>
2023-05-12 16:53:54 +0100

    chore: update comment on useRecommendedProducts

--- packages/osc-ecommerce/app/hooks/useRecommendedProducts.ts
+++ packages/osc-ecommerce/app/hooks/useRecommendedProducts.ts
@@ -2,6 +2,14 @@ import type { RouteMatch } from '@remix-run/react';
 import type { Product } from '@shopify/hydrogen/storefront-api-types';
 import { useMatches } from 'react-router-dom';
 
+/**
+ * A custom hook that returns the recommended products from the course page.
+ *
+ * The recommended products must be exported from the loader.
+ * Can only be used on a course page.
+ *
+ * @returns The recommended products from the course page.
+ */
 export const useRecommendedProducts = () => {
     const matches = useMatches();
     const match = matches.find((match) => match.id === 'routes/courses/$slug') as RouteMatch;
