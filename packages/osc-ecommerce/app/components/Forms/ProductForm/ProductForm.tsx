c43db6572e6156b43738d28cea96413857bf86e7
Steven Bister <steve.bister@openstudycollege.com>
2023-05-19 16:11:06 +0100

    refactor(add to cart): move add to cart into cart actions directory

--- packages/osc-ecommerce/app/components/Forms/ProductForm/ProductForm.tsx
+++ packages/osc-ecommerce/app/components/Forms/ProductForm/ProductForm.tsx
@@ -11,7 +11,7 @@ import { AlreadyInCartMessage } from '~/components/Cart/AlreadyInCartMessage';
 import { Price } from '~/components/Price/Price';
 import { useCart } from '~/hooks/useCart';
 import { isGiftVoucher } from '~/utils/storefront.helpers';
-import { AddToCart } from '../AddToCart/AddToCart';
+import { AddToCart } from '../CartActions/AddToCart';
 
 interface Product {
     product: ProductType & { selectedVariant?: ProductVariant };
