5a67d09110a1ccbcc6a7b623f398514cb68a9a45
Steven Bister <steve.bister@openstudycollege.com>
2023-05-19 15:29:30 +0100

    refactor(cart): move cart layout into own component

---
+++ packages/osc-ecommerce/app/components/Cart/Layout.tsx
@@ -0,0 +1,101 @@
+import { flattenConnection } from '@shopify/hydrogen';
+import { mediaQueries as mq } from 'osc-design-tokens';
+import { Button, Card, CardBody, CardFooter, CardTitle, Icon, rem, useMediaQuery } from 'osc-ui';
+import { useEffect, useState } from 'react';
+import { CartCardItem } from '~/components/Cart/CartCardItem';
+import { CartTotal } from '~/components/Cart/CartTotal';
+import { CartLineItem } from '~/components/Cart/LineItem';
+import { PATHS } from '~/constants';
+import { useCart } from '~/hooks/useCart';
+
+export const CartLayout = () => {
+    const cart = useCart();
+    const linesCount = Boolean(cart?.lines?.edges?.length || 0);
+
+    const cartLines = linesCount && cart?.lines ? flattenConnection(cart?.lines) : [];
+
+    console.log('cart', cart);
+
+    const isGreaterThanTab = useMediaQuery(`(min-width: ${rem(mq.tab)}rem)`);
+    const [showOnGreaterThanTab, setShowOnGreaterThanTab] = useState(false);
+
+    // We need this useEffect to set the showOnTab state only when the window object exists
+    // Otherwise we will receive an SSR warning telling us the markup differs from the server
+    useEffect(() => {
+        setShowOnGreaterThanTab(isGreaterThanTab);
+    }, [isGreaterThanTab]);
+
+    return (
+        <>
+            <header className="o-container o-grid">
+                <div className="o-grid__col o-grid__col--11 o-grid__col--start-2@tab">
+                    <h1 className="t-font-secondary t-font-5xl u-pt-2xl">
+                        Your bag {linesCount ? '' : <>is empty</>}
+                    </h1>
+                </div>
+            </header>
+
+            <div className="o-container o-grid u-pb-6xl">
+                <div className="o-grid__col o-grid__col--12 o-grid__col--6@tab o-grid__col--start-2@tab u-hidden-until@tab">
+                    {!linesCount ? (
+                        // TODO: Can make this text CMS editable
+                        <p>
+                            We have more than 750 courses and qualifications to choose from,
+                            continue browsing and join our family of over 110,000 students today.
+                        </p>
+                    ) : null}
+
+                    {linesCount && showOnGreaterThanTab ? (
+                        <>
+                            <ul>
+                                {cartLines.map((line) => {
+                                    if (!line.id) return null;
+
+                                    return <CartCardItem line={line} key={line.id} />;
+                                })}
+                            </ul>
+                        </>
+                    ) : null}
+
+                    <Button
+                        as="link"
+                        to={`/${PATHS.WISHLIST}`}
+                        variant="tertiary"
+                        className="u-mt-m"
+                    >
+                        View Wishlist <Icon id="heart" />
+                    </Button>
+                </div>
+
+                {linesCount ? (
+                    <div className="o-grid__col o-grid__col--12 o-grid__col--4@tab">
+                        <Card hasShadow className="u-pt-m u-pr-l u-pl-l u-pb-2xl u-h-auto">
+                            <CardTitle isUnderlined>Total</CardTitle>
+                            <CardBody>
+                                <ul>
+                                    {cartLines.map((line) => {
+                                        if (!line.id) return null;
+
+                                        return showOnGreaterThanTab ? (
+                                            <CartLineItem line={line} key={line.id} />
+                                        ) : (
+                                            <CartCardItem line={line} key={line.id} />
+                                        );
+                                    })}
+                                </ul>
+
+                                <CartTotal cost={cart.cost} />
+                            </CardBody>
+
+                            <CardFooter>
+                                <Button as="a" href={cart.checkoutUrl} isFull>
+                                    Enrol now
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                ) : null}
+            </div>
+        </>
+    );
+};
--- packages/osc-ecommerce/app/routes/cart.tsx
+++ packages/osc-ecommerce/app/routes/cart.tsx
@@ -1,6 +1,5 @@
 import type { ActionArgs, LinksFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { flattenConnection } from '@shopify/hydrogen';
 import type {
     CartBuyerIdentityInput,
     CartLineInput,
@@ -8,23 +7,21 @@ import type {
     CartUserError,
     UserError,
 } from '@shopify/hydrogen/storefront-api-types';
-import { Button, Card, CardBody, CardFooter, CardTitle, Icon } from 'osc-ui';
+import accordionStyles from 'osc-ui/dist/src-components-Accordion-accordion.css';
 import buttonStyles from 'osc-ui/dist/src-components-Button-button.css';
 import cardStyles from 'osc-ui/dist/src-components-Card-card.css';
 import lineItemStyles from 'osc-ui/dist/src-components-LineItem-line-item.css';
 import selectStyles from 'osc-ui/dist/src-components-Select-select.css';
 import invariant from 'tiny-invariant';
-import { CartCardItem } from '~/components/Cart/CartCardItem';
-import { CartTotal } from '~/components/Cart/CartTotal';
-import { CartLineItem } from '~/components/Cart/LineItem';
-import { PATHS } from '~/constants';
-import { useCart } from '~/hooks/useCart';
+import { CartLayout } from '~/components/Cart/Layout';
+
 import type { CartActions } from '~/types/shopify';
 import { CartAction } from '~/types/shopify';
 import { addLinesToCart, createCart } from '~/utils/cart.helpers';
 
 export const links: LinksFunction = () => {
     return [
+        { rel: 'stylesheet', href: accordionStyles },
         { rel: 'stylesheet', href: buttonStyles },
         { rel: 'stylesheet', href: cardStyles },
         { rel: 'stylesheet', href: lineItemStyles },
@@ -117,67 +114,5 @@ export async function action({ request, context }: ActionArgs) {
 }
 
 export default function CartRoute() {
-    const cart = useCart();
-    const linesCount = Boolean(cart?.lines?.edges?.length || 0);
-
-    const cartLines = linesCount && cart?.lines ? flattenConnection(cart?.lines) : [];
-
-    return (
-        <>
-            <header className="o-container o-grid">
-                <div className="o-grid__col o-grid__col--11 o-grid__col--start-2">
-                    <h1 className="t-font-secondary t-font-5xl u-pt-2xl">
-                        Your bag {linesCount ? '' : <>is empty</>}
-                    </h1>
-                </div>
-            </header>
-
-            <div className="o-container o-grid u-pb-6xl">
-                <div className="o-grid__col o-grid__col--6 o-grid__col--start-2">
-                    {linesCount ? (
-                        <ul>
-                            {cartLines.map((line) => {
-                                if (!line.id) return null;
-
-                                return <CartCardItem line={line} key={line.id} />;
-                            })}
-                        </ul>
-                    ) : (
-                        // TODO: Can make this text CMS editable
-                        <p>
-                            We have more than 750 courses and qualifications to choose from,
-                            continue browsing and join our family of over 110,000 students today.
-                        </p>
-                    )}
-
-                    <Button as="link" to={`/${PATHS.WISHLIST}`} variant="tertiary">
-                        View Wishlist <Icon id="heart" />
-                    </Button>
-                </div>
-
-                {linesCount ? (
-                    <div className="o-grid__col o-grid__col--4">
-                        <Card hasShadow className="u-pt-m u-pr-l u-pl-l u-pb-2xl u-h-auto">
-                            <CardTitle isUnderlined>Total</CardTitle>
-                            <CardBody>
-                                <ul>
-                                    {cartLines.map((line) => {
-                                        if (!line.id) return null;
-
-                                        return <CartLineItem line={line} key={line.id} />;
-                                    })}
-                                </ul>
-
-                                <CartTotal cost={cart.cost} />
-                            </CardBody>
-
-                            <CardFooter>
-                                <Button isFull>Enrol now</Button>
-                            </CardFooter>
-                        </Card>
-                    </div>
-                ) : null}
-            </div>
-        </>
-    );
+    return <CartLayout />;
 }
