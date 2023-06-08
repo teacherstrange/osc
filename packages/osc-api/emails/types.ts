7e863ccac5b325fbccbfb5ac980b5db6779976a1
hall2521 <hall173@hotmail.com>
2023-06-08 13:18:29 +0100

    boolean type

--- packages/osc-api/emails/types.ts
+++ packages/osc-api/emails/types.ts
@@ -31,5 +31,5 @@ type emailData = {
     customProperties: custom;
 };
 
-export type GetEmailData = (input: emailData) => Promise<boolean | Error>;
-export type GetRegEmailData = (input: regEmailData) => Promise<boolean | Error>;
+export type GetEmailData = (input: emailData) => Promise<Boolean | Error>;
+export type GetRegEmailData = (input: regEmailData) => Promise<Boolean | Error>;
