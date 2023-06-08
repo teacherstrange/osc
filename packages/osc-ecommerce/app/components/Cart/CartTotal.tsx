9f11c1fbc8cfe11858ac1276064ae1efe4c97103
Steven Bister <steve.bister@openstudycollege.com>
2023-06-08 13:11:26 +0100

    feat(cart): add discounted values to cart total

--- packages/osc-ecommerce/app/components/Cart/CartTotal.tsx
+++ packages/osc-ecommerce/app/components/Cart/CartTotal.tsx
@@ -10,23 +10,56 @@ interface CartTotalProps {
 export const CartTotal = (props: CartTotalProps) => {
     const { cost, isLoading } = props;
 
-    return (
-        <LineItem variant="tertiary" asChild>
-            <div data-anim={isLoading ? 'shimmer' : ''}>
-                <LineItemHeader className="t-font-l u-text-bold u-mb-0">Total</LineItemHeader>
+    const { totalAmount, subtotalAmount } = cost;
 
-                {cost?.totalAmount?.amount ? (
-                    <LineItemPrice asChild>
-                        <Price>
-                            <Money
-                                data={cost?.totalAmount}
-                                as="span"
-                                className="t-font-l u-text-bold"
-                            />
-                        </Price>
-                    </LineItemPrice>
-                ) : null}
-            </div>
-        </LineItem>
+    const hasCartDiscount =
+        totalAmount.amount && subtotalAmount.amount && totalAmount.amount < subtotalAmount.amount;
+
+    return (
+        <>
+            <LineItem variant="tertiary" asChild>
+                <div data-anim={isLoading ? 'shimmer' : ''}>
+                    <LineItemHeader className="t-font-m u-mb-0 u-color-neutral-700 u-text-reg">
+                        Subtotal
+                    </LineItemHeader>
+
+                    {subtotalAmount?.amount ? (
+                        <LineItemPrice asChild>
+                            <Price size="sm">
+                                <Money
+                                    data={subtotalAmount}
+                                    as="span"
+                                    className="t-font-m u-color-neutral-700 u-text-bold"
+                                />
+                            </Price>
+                        </LineItemPrice>
+                    ) : null}
+                </div>
+            </LineItem>
+
+            <LineItem variant="tertiary" asChild>
+                <div data-anim={isLoading ? 'shimmer' : ''}>
+                    <LineItemHeader className="t-font-l u-text-bold u-mb-0">Total</LineItemHeader>
+
+                    {totalAmount?.amount ? (
+                        <LineItemPrice asChild>
+                            <Price
+                                compareAtPrice={
+                                    hasCartDiscount ? (
+                                        <Money data={subtotalAmount} as="span" />
+                                    ) : null
+                                }
+                            >
+                                <Money
+                                    data={totalAmount}
+                                    as="span"
+                                    className="t-font-l u-text-bold"
+                                />
+                            </Price>
+                        </LineItemPrice>
+                    ) : null}
+                </div>
+            </LineItem>
+        </>
     );
 };
