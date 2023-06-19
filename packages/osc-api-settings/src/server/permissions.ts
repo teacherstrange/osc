6861bbaf4035c28bcc0b947814ec182084cd67c2
Tom Pearce <thomas.pearce@openstudycollege.com>
2023-06-19 14:33:21 +0100

    feat(settings-api): added shield permission rules

--- packages/osc-api-settings/src/server/permissions.ts
+++ packages/osc-api-settings/src/server/permissions.ts
@@ -1,5 +1,16 @@
 import { shield } from 'graphql-shield';
+import { isAuthenticated } from './rules';
 
 export const shieldPermissions = shield({
-    Query: {},
+    Query: {
+        preference: isAuthenticated,
+        preferenceByKey: isAuthenticated,
+        userPreferences: isAuthenticated,
+        userPreferenceByKey: isAuthenticated,
+        userPreference: isAuthenticated,
+    },
+    Mutation: {
+        savePreference: isAuthenticated,
+        savePreferenceByKey: isAuthenticated,
+    },
 });
